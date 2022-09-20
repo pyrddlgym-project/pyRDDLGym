d4aa4210a2aaf2712807a9b81b8a423eb92b0423
Ram <marirsg2@gmail.com>
2022-09-20 19:16:48 -0400

    test mod to run Ram's grounder

--- test.py
+++ test.py
@@ -40,57 +40,52 @@ def main():
     # MyXADDTranslator.Translate()
 
     from pprint import pprint
-    grounder = RDDLGrounder.RDDLGroundedGrounder(rddl_ast)
-    model = grounder.Ground()
-    pprint(vars(model))
-    
-    good_policy = True
-    sampler = RDDLSimulator(model)
-    loops = 1
-    
-    print('\nstarting simulation')
-    for h in range(loops):
-        state = sampler.reset_state()
-        total_reward = 0.
-        for _ in range(2000):
-            sampler.check_state_invariants()
-            sampler.check_action_preconditions()
-            actions = {'AIR_r1': 0., 'AIR_r2': 0., 'AIR_r3': 0.}
-            if good_policy:
-                if state['TEMP_r1'] < 20.5:
-                    actions['AIR_r1'] = 5.
-                if state['TEMP_r2'] < 20.5:
-                    actions['AIR_r2'] = 5.
-                if state['TEMP_r3'] < 20.5:
-                    actions['AIR_r3'] = 5.
-                if state['TEMP_r1'] > 23.:
-                    actions['AIR_r1'] = 0.
-                if state['TEMP_r2'] > 23.:
-                    actions['AIR_r2'] = 0.
-                if state['TEMP_r3'] > 23.:
-                    actions['AIR_r3'] = 0.
-            state = sampler.sample_next_state(actions)
-            reward = sampler.sample_reward()
-            sampler.update_state()
-            if h == 0:
-                print('state = {}'.format(state))
-                print('reward = {}'.format(reward))
-                print('derived = {}'.format(model.derived))
-                print('interm = {}'.format(model.interm))
-                print('')
-            total_reward += reward
-        print('trial {}, total reward {}'.format(h, total_reward))
+    # grounder = RDDLGrounder.RDDLGroundedGrounder(rddl_ast)
+    # model = grounder.Ground()
+    # pprint(vars(model))
+    #
+    # good_policy = True
+    # sampler = RDDLSimulator(model)
+    # loops = 1
+    #
+    # print('\nstarting simulation')
+    # for h in range(loops):
+    #     state = sampler.reset_state()
+    #     total_reward = 0.
+    #     for _ in range(2000):
+    #         sampler.check_state_invariants()
+    #         sampler.check_action_preconditions()
+    #         actions = {'AIR_r1': 0., 'AIR_r2': 0., 'AIR_r3': 0.}
+    #         if good_policy:
+    #             if state['TEMP_r1'] < 20.5:
+    #                 actions['AIR_r1'] = 5.
+    #             if state['TEMP_r2'] < 20.5:
+    #                 actions['AIR_r2'] = 5.
+    #             if state['TEMP_r3'] < 20.5:
+    #                 actions['AIR_r3'] = 5.
+    #             if state['TEMP_r1'] > 23.:
+    #                 actions['AIR_r1'] = 0.
+    #             if state['TEMP_r2'] > 23.:
+    #                 actions['AIR_r2'] = 0.
+    #             if state['TEMP_r3'] > 23.:
+    #                 actions['AIR_r3'] = 0.
+    #         state = sampler.sample_next_state(actions)
+    #         reward = sampler.sample_reward()
+    #         sampler.update_state()
+    #         if h == 0:
+    #             print('state = {}'.format(state))
+    #             print('reward = {}'.format(reward))
+    #             print('derived = {}'.format(model.derived))
+    #             print('interm = {}'.format(model.interm))
+    #             print('')
+    #         total_reward += reward
+    #     print('trial {}, total reward {}'.format(h, total_reward))
         
-    # grounder = RDDLGrounder.RDDLGrounder(rddl_ast)
-    # grounder.Ground()
+    grounder = RDDLGrounder.RDDLGrounder(rddl_ast)
+    grounder.Ground()
     # pprint(vars(grounder))
     
     # grounder.InitGround()
-
-    grounder2 = RDDLGrounder.RDDLGrounder(rddl_ast)
-    model2 = grounder2.Ground()
-
-
     # generator = RDDLGenerator.RDDLGenerator(rddl_ast)
     # rddl = generator.GenerateRDDL()
     
