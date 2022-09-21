dc275f6cd2dffe9d7f9830ee5d74bdd721678008
Ayal Taitler <ataitler@gmail.com>
2022-09-21 19:23:31 -0400

    support added for exposing instance objects in the Model object and in the groundergrounder

--- Grounder/RDDLGrounder.py
+++ Grounder/RDDLGrounder.py
@@ -27,6 +27,7 @@ class RDDLGroundedGrounder(Grounder):
         self._cpforder = {0 : []}
         self._derived = {}
         self._interm = {}
+        self._objects = {}
 
         self._reward = None
         self._preconditions = []
@@ -41,6 +42,8 @@ class RDDLGroundedGrounder(Grounder):
 
         # initialize the Model object
         model = RDDLModel()
+
+        self._getObjects()
 
         # ground pvariables and appropriate cpfs if applicable
         # update pvariables
@@ -79,9 +82,18 @@ class RDDLGroundedGrounder(Grounder):
         model.discount = self._groundDiscount()
         model.actionsranges = self._actionsranges
         model.statesranges = self._statesranges
+        model.objects = self._objects
         # new properties
 
         return model
+
+    def _getObjects(self):
+        self._objects = {}
+        try:
+            for type in self._AST.non_fluents.objects:
+                self._objects[type[0]] = type[1]
+        except:
+            return
 
     def _groundHorizon(self):
         return self._AST.instance.horizon
