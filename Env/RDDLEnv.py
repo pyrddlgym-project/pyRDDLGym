6e877d38438f1e2fd7d3a82c120005a0d74b655d
Ayal Taitler <ataitler@gmail.com>
2022-10-03 16:25:31 -0400

    constraints handling (states/actions) in gym env

--- Env/RDDLEnv.py
+++ Env/RDDLEnv.py
@@ -1,20 +1,23 @@
 import copy
+import sys
+import warnings
 
 import gym
 from gym.spaces import Discrete, Dict, Box
 import numpy as np
 
+import Simulator.RDDLSimulator
 from Parser import parser as parser
 from Parser import RDDLReader as RDDLReader
 import Grounder.RDDLGrounder as RDDLGrounder
-from Simulator.RDDLSimulator import RDDLSimulator
+from Simulator.RDDLSimulator import RDDLSimulatorWConstraints
 
 class RDDLEnv(gym.Env):
     def __init__(self, domain, instance=None):
         super(RDDLEnv, self).__init__()
 
         # max allowed action value
-        self.BigM = 10
+        self.BigM = 100
 
         # read and parser domain and instance
         if instance is None:
@@ -35,7 +38,7 @@ class RDDLEnv(gym.Env):
         self.model = grounder.Ground()
 
         # define the model sampler
-        self.sampler = RDDLSimulator(self.model)
+        self.sampler = RDDLSimulatorWConstraints(self.model, max_bound=self.BigM)
 
         # set the horizon
         self.horizon = self.model.horizon
@@ -50,13 +53,16 @@ class RDDLEnv(gym.Env):
         # set default actions dic
         self.defaultAction = copy.deepcopy(self.model.actions)
 
-        #TODO
-        # define the action correct bounds as specified in the RDDL file
+        # define the actions bounds
         action_space = Dict()
         for act in self.model.actions:
             range = self.model.actionsranges[act]
             if range == 'real':
-                action_space[act] = Box(low=-self.BigM, high=self.BigM, dtype=np.float32)
+                if act in self.sampler.bounds:
+                    action_space[act] = Box(low=self.sampler.bounds[act][0], high=self.sampler.bounds[act][1],
+                                            dtype=np.float32)
+                else:
+                    action_space[act] = Box(low=-self.BigM, high=self.BigM, dtype=np.float32)
             elif range == 'bool':
                 action_space[act] = Discrete(2)
             elif range == 'int':
@@ -65,13 +71,16 @@ class RDDLEnv(gym.Env):
                 raise Exception("unknown action range in gym environment init")
         self.action_space = action_space
 
-        #TODO
-        # define the observation space correct bounds as specified in the RDDL file
+        # define the states bounds
         state_space = Dict()
         for state in self.model.states:
             range = self.model.statesranges[state]
             if range == 'real':
-                state_space[state] = Box(low=-self.BigM, high=self.BigM, dtype=np.float32)
+                if state in self.sampler.bounds:
+                    state_space[state] = Box(low=self.sampler.bounds[state][0], high=self.sampler.bounds[state][1],
+                                             dtype=np.float32)
+                else:
+                    state_space[state] = Box(low=-self.BigM, high=self.BigM, dtype=np.float32)
             elif range == 'bool':
                 state_space[state] = Discrete(2)
             elif range == 'int':
@@ -93,29 +102,30 @@ class RDDLEnv(gym.Env):
                 "Invalid action, expected maximum of {} entries, {} were were given".format(self._NumConcurrentActions,
                                                                                             action_length))
 
-        # check that all actions preconditions are satisfied
-        try:
-            self.sampler.check_action_preconditions()
-        except:
-
-
-        # set full action vector
+        # set full action vector, values are clipped to be inside the feasible action space
         action = copy.deepcopy(self.defaultAction)
         for act in at:
-            action[act] = at[act]
+            action[act] = self.clip(at[act], self.sampler.bounds[act][0], self.sampler.bounds[act][1])
 
         # sample next state and reward
         state = self.sampler.sample_next_state(action)
         reward = self.sampler.sample_reward()
 
         # check if the state is within the invariant constraints
+        for st in state:
+            if self.model.statesranges[st] == 'real':
+                state[st] = self.clip(state[st], self.sampler.bounds[st][0], self.sampler.bounds[st][1])
+
+        self.sampler.update_state(state)
+
+        # check for non-linear constraint violation
         try:
             self.sampler.check_state_invariants()
-            self.sampler.update_state()
-        except:
-            # update state with corrected values
-            self.sampler.update_state()
-
+        except Exception:
+            if isinstance(sys.exc_info()[1], Simulator.RDDLSimulator.RDDLRuntimeError):
+                print("WARNING:",sys.exc_info()[1])
+            else:
+                raise Exception("Error in state constraint validation").with_traceback(sys.exc_info()[2])
 
         # update step horizon
         self.currentH += 1
@@ -129,9 +139,7 @@ class RDDLEnv(gym.Env):
     def reset(self):
         self.total_reward = 0
         self.currentH = 0
-        # self.state = self.sampler.reset_state()
         return self.sampler.reset_state()
-        # return self.state
 
     def render(self):
         pass
@@ -139,3 +147,6 @@ class RDDLEnv(gym.Env):
     @property
     def NumConcurrentActions(self):
         return self._NumConcurrentActions
+
+    def clip(self, val, low, high):
+        return max(min(val, high), low)
