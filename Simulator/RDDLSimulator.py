bcf72a82a967dd6434b34c414a3b7b7ead5cf10c
Mike Gimelfarb <35513382+mike-gimelfarb@users.noreply.github.com>
2022-10-10 20:01:55 -0400

    Better error handling
    - error inherit type fix
    - better debugging of div
    - function types raise exceptions for invalid args

--- Simulator/RDDLSimulator.py
+++ Simulator/RDDLSimulator.py
@@ -25,7 +25,7 @@ class RDDLUndefinedVariableError(SyntaxError):
     pass
 
 
-class RDDLInvalidNumberOfArgumentsError(ValueError):
+class RDDLInvalidNumberOfArgumentsError(SyntaxError):
     pass
 
 
@@ -295,19 +295,21 @@ class RDDLSimulator:
         elif op == '*':
             return arg1 * arg2
         elif op == '/':
-            return RDDLSimulator._ieee754_division(arg1, arg2) 
+            return RDDLSimulator._ieee754_division(expr, arg1, arg2) 
         else:
             raise RDDLNotImplementedError(
                 'Arithmetic operator {} is not supported.'.format(op) + 
                 '\n' + RDDLSimulator._print_stack_trace(expr))
     
     @staticmethod
-    def _ieee754_division(arg1, arg2):  # TODO: is nan valid
+    def _ieee754_division(expr, arg1, arg2):
         if arg1 != arg1 or arg2 != arg2:
-            raise ArithmeticError('NaN value encountered.')
+            raise ArithmeticError('NaN value encountered in division.' + 
+                                  '\n' + RDDLSimulator._print_stack_trace(expr))
         elif arg2 == 0:
             if arg1 == 0:
-                raise ArithmeticError('NaN value encountered.')
+                raise ArithmeticError('NaN value encountered in division.' + 
+                                      '\n' + RDDLSimulator._print_stack_trace(expr))
             elif arg1 < 0:
                 return -math.inf
             else:
@@ -375,7 +377,12 @@ class RDDLSimulator:
                             
             arg = self._sample(args[0], subs)
             arg = 1 * arg  # bool -> int
-            return RDDLSimulator.KNOWN_UNARY[name](arg)
+            try:
+                return RDDLSimulator.KNOWN_UNARY[name](arg)
+            except:
+                raise RDDLValueOutOfRangeError(
+                    'Function {} could not be evaluated with arg {}.'.format(name, arg) + 
+                    '\n' + RDDLSimulator._print_stack_trace(expr))
         
         elif name in RDDLSimulator.KNOWN_BINARY:
             if len(args) != 2:
@@ -387,7 +394,13 @@ class RDDLSimulator:
             arg2 = self._sample(args[1], subs)
             arg1 = 1 * arg1  # bool -> int
             arg2 = 1 * arg2
-            return RDDLSimulator.KNOWN_BINARY[name](arg1, arg2)
+            try:
+                return RDDLSimulator.KNOWN_BINARY[name](arg1, arg2)
+            except:
+                raise RDDLValueOutOfRangeError(
+                    'Function {} could not be evaluated with args {} and {}.'.format(
+                        name, arg1, arg2) + 
+                    '\n' + RDDLSimulator._print_stack_trace(expr))
         
         else:
             raise RDDLNotImplementedError(
