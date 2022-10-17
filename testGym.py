964a1c79de5789ec70fbb7862f24f05bd4e8c0fd
Ayal Taitler <ataitler@gmail.com>
2022-10-16 23:53:28 -0400

    parsing bounds bug fix, and np.inf instead of BigM boung

--- testGym.py
+++ testGym.py
@@ -1,21 +1,21 @@
 from Env import RDDLEnv as RDDLEnv
-from Policies.Agents import RandomAgentsdfs
+from Policies.Agents import RandomAgent
 
 
 # PROBLEM = 'RDDL/Thiagos_HVAC_grounded.rddl'
 # PROBLEM = 'RDDL/Thiagos_Mars_Rover.rddl'
 # PROBLEM = 'RDDL/Thiagos_HVAC.rddl'
-# FOLDER = 'Competition/Power_gen/'
+FOLDER = 'Competition/Power_gen/'
 # FOLDER = 'Competition/Mars_rover/'
-FOLDER = 'Competition/drone_con/'
+# FOLDER = 'Competition/drone_mix/'
 
 def main():
-    myEnv = RDDLEnv.RDDLEnv(domain=FOLDER+'domain.rddl', instance=FOLDER+'instance1.rddl', is_grounded=False)
+    myEnv = RDDLEnv.RDDLEnv(domain=FOLDER+'domain.rddl', instance=FOLDER+'instance0.rddl', is_grounded=False)
     agent = RandomAgent(action_space=myEnv.action_space, num_actions=myEnv.NumConcurrentActions)
 
     total_reward = 0
     state = myEnv.reset()
-    for step in range(myEnv.horizon):
+    for step in range(myEnv.horizon+5):
         myEnv.render()
         action = agent.sample_action()
 
