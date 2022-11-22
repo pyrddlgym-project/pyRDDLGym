Fixes to jax compiler and backprop
- relational fixes in compiler for enforce_diff
- planner now logs best plan, and returns more information
-

pyRDDLGym/Core/Jax/JaxRDDLCompiler.py
@@ -330,6 +330,10 @@ class JaxRDDLCompiler:
             '~=': jnp.not_equal
         },
         True: {
+            '>=': jnp.greater_equal,
+            '<=': jnp.less_equal,
+            '<': jnp.less,
+            '>': jnp.greater
         }
     }
     
@@ -358,7 +362,7 @@ class JaxRDDLCompiler:
         lhs, rhs = args
         jax_lhs = self._jax(lhs, params)
         jax_rhs = self._jax(rhs, params)
-        jax_op = JaxRDDLCompiler.RELATIONAL_OPS[op]
+        jax_op = OPS[op]
         return JaxRDDLCompiler._jax_binary(jax_lhs, jax_rhs, jax_op)
         
     # logical expressions    

pyRDDLGym/Core/Jax/JaxRDDLStraightlinePlanner.py
@@ -3,6 +3,7 @@ import jax
 import jax.numpy as jnp
 import jax.random as random
 import optax
+from typing import Dict, Generator
 
 from pyRDDLGym.Core.Jax.JaxRDDLCompiler import JaxRDDLCompiler
 from pyRDDLGym.Core.Jax.JaxRDDLSimulator import JaxRDDLSimulator
@@ -76,13 +77,16 @@ class JaxRDDLStraightlinePlanner:
         
         # force action ranges         
         def _force_action_ranges(plan):
+            new_plan = {}
             for name, value in plan.items():
                 atype, _ = action_info[name]
                 if atype == JaxRDDLCompiler.INT:
-                    plan[name] = jnp.asarray(value, dtype=JaxRDDLCompiler.REAL)
+                    new_plan[name] = jnp.asarray(value, dtype=JaxRDDLCompiler.REAL)
                 elif atype == bool:
-                    plan[name] = jax.nn.sigmoid(value)
-            return plan
+                    new_plan[name] = jax.nn.sigmoid(value)
+                else:
+                    new_plan[name] = value
+            return new_plan
         
         self.force_action_ranges = jax.jit(_force_action_ranges)
         
@@ -129,18 +133,33 @@ class JaxRDDLStraightlinePlanner:
         
         self.initialize = jax.jit(_initialize)
 
-    def optimize(self, n_epochs: int):
+    def optimize(self, n_epochs: int) -> Generator[Dict, None, None]:
         ''' Compute an optimal straight-line plan for the RDDL domain and instance.
         
         @param n_epochs: the maximum number of steps of gradient descent
         '''
         plan, opt_state, self.key = self.initialize(self.key)
-
+        
+        best_plan = self.force_action_ranges(plan)
+        best_loss = float('inf')
+        
         for step in range(n_epochs):
             plan, opt_state, self.key, _, _ = self.update(plan, opt_state, self.key)
-            loss_val, (self.key, x_batch, errs) = self.loss(plan, self.key)
-            
-            fixed_plan = self.force_action_ranges(plan)
+                       
+            loss_val, (self.key, rollouts, errs) = self.loss(plan, self.key)
             errs = JaxRDDLCompiler.get_error_codes(errs)
-            yield step, fixed_plan, loss_val, x_batch, errs
+            fixed_plan = self.force_action_ranges(plan)
+
+            if loss_val < best_loss:
+                best_plan = fixed_plan
+                best_loss = loss_val
+            
+            callback = {'step': step,
+                        'plan': fixed_plan,
+                        'best_plan': best_plan,
+                        'loss': loss_val,
+                        'best_loss': best_loss,
+                        'rollouts': rollouts,
+                        'errors': errs}
+            yield callback
         

pyRDDLGym/JaxExample.py
@@ -1,7 +1,7 @@
 import jax
 jax.config.update('jax_log_compiles', True)
 import numpy as np
-
+import optax  
 from pyRDDLGym import ExampleManager
 
 from pyRDDLGym.Core.Env.RDDLEnv import RDDLEnv
@@ -11,19 +11,19 @@ from pyRDDLGym.Core.Parser import parser as parser
 from pyRDDLGym.Core.Parser.RDDLReader import RDDLReader
 
 
-# ENV = 'PowerGeneration'
-# ENV = 'MarsRover'
-# ENV = 'UAV continuous'
+#ENV = 'PowerGeneration'
+#ENV = 'MarsRover'
+ENV = 'UAV continuous'
 # ENV = 'UAV discrete'
 # ENV = 'UAV mixed'
 # ENV = 'MountainCar'
-ENV = 'Reservoir'
+#ENV = 'PowerGeneration'
 # ENV = 'CartPole discrete'
-# ENV = 'Elevators'
-# ENV = 'RaceCar'
+#ENV = 'Elevators'
+ENV = 'Reservoir'
 # ENV = 'RecSim'
 
-DO_PLAN = False
+DO_PLAN = True
 
 
 def main():
@@ -37,21 +37,26 @@ def main():
     ast = MyRDDLParser.parse(domain)   
     
     # parse RDDL file 
-    key = jax.random.PRNGKey(42)
+    key = jax.random.PRNGKey(np.random.randint(0, 2 ** 31))
     if DO_PLAN:
         
-        planner = JaxRDDLStraightlinePlanner(ast, key, 20, 2048)
-        for step, _, loss, err in planner.optimize(100):
-            print('step={} loss={} err={}'.format(str(step).rjust(4), loss, err))
-    
+        planner = JaxRDDLStraightlinePlanner(ast, key, 20, 2048, 
+                                             optimizer=optax.adam(0.5),
+                                             initializer=jax.nn.initializers.normal())
+        for callback in planner.optimize(500):
+            print('step={} loss={:.4f} best_loss={:.4f} err={}'.format(
+                str(callback['step']).rjust(4), callback['loss'], 
+                callback['best_loss'], callback['errors']))
+        print(callback['best_plan'])
+        
     else:
         
-        sim = JaxRDDLSimulator(ast, key=key)
+        sim = JaxRDDLSimulator(ast, key=key, enforce_diff=True)
         
         total_reward = 0
         state, done = sim.reset() 
-        for step in range(10):
-            action = {'flow': np.array([.1, .2, .3])}
+        for step in range(100):
+            action = {}
             next_state, reward, done = sim.step(action)
             
             print()