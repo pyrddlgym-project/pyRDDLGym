967e9ee048c7a940d965b41fc615fb9c038ad9ea
Mike Gimelfarb <35513382+mike-gimelfarb@users.noreply.github.com>
2023-01-29 15:31:40 -0500

    Automatic manifest creation for ExampleManager
    - now a domain.info file needs to be created and it will be scanned automatically in the Examples subdirectory tree
    - a manifest can be created by calling Manifest.create()
    - this will be used in ExampleManager instead of hard coded dictionary

--- pyRDDLGym/Examples/ExampleManager.py
+++ pyRDDLGym/Examples/ExampleManager.py
@@ -1,37 +1,39 @@
 import os
 import re
 from pyRDDLGym.Core.ErrorHandling.RDDLException import RDDLEnvironmentNotExist, RDDLInstanceNotExist
+from pyRDDLGym.Examples import Manifest
 
-EXP_DICT = {
-    'CartPole discrete' : ('A simple continuous state MDP for the classical cart-pole system by Rich Sutton, '
-                           'with discrete actions that apply a constant force on either the left ' 
-                           'or right side of the cart.', '/CartPole/Discrete/', 'CartPole'),
-    'CartPole continuous' : ('A simple continuous state-action MDP for the classical cart-pole system by Rich Sutton, '
-                             'with actions that describe the continuous force applied to the cart.', '/CartPole/Continuous/', 'CartPole'),
-    'Elevators' : ('The Elevator domain models evening rush hours when people from different floors in '
-                   'a building want to go down to the bottom floor using elevators', '/Elevator/', 'Elevator'),
-    'MarsRover' : ('Multi Rover Navigation, where a group of agent needs to harvest mineral.', '/Mars_rover/', 'MarsRover'),
-    'MountainCar' : ('A simple continuous MDP for the classical mountain car control problem.', '/MountainCar/', 'MountainCar'),
-    'PowerGeneration' : ('A simple power generation problem loosely modeled on the problem of unit commitment.',
-                          '/Power_gen/', 'PowerGen'),
-    'RaceCar' : ('A simple continuous MDP for the racecar problem.', '/Racecar/', 'Racecar'),
-    'RecSim' : ('A problem of recommendation systems, with consumers and providers.', '/Recsim/', 'RecSim'),
-    'UAV continuous' : ('Continous action space version of multi-UAV problem where a group of UAVs have to reach goal '
-                        'positions in  in the 3d Space.', '/UAV/Continuous/', 'UAVs'),
-    'UAV discrete' : ('Discrete action space version of multi-UAV problem where a group of UAVs have to reach goal '
-    'positions in  in the 3d Space.', '/UAV/Discrete/', 'UAVs'),
-    'UAV mixed' : ('Mixed action space version of multi-UAV problem where a group of UAVs have to reach goal '
-    'positions in  in the 3d Space.', '/UAV/Mixed/', 'UAVs'),
-    'Wildfire' : ('A boolean version of the wildfire fighting domain.', '/Wildfire/', 'Wildfire'),
-    'SupplyChain' : ('A supply chain with factory and multiple warehouses.', '/Supply_Chain/', 'SupplyChain'),
-    'Traffic' : ('BLX/QTM traffic model.', '/Traffic/', None),
-    'PropDBN' : ('Simple propositional DBN.', '/PropDBN/', None),
-    'WildlifePreserve' : ('Complex domain from IPPC 2018.', '/WildlifePreserve/', None),
-    'NewLanguage' : ('Example with new language features.', '/NewLanguageExamples/NewLanguage/', None),
-    'NewtonZero' : ('Example with Newton root-finding method.', '/NewLanguageExamples/NewtonZero/', None),
-    'Reservoir': ('Managing the water level in interconnected reservoirs.', '/Reservoir/', None)
-}
+# EXP_DICT = {
+#     'CartPole discrete' : ('A simple continuous state MDP for the classical cart-pole system by Rich Sutton, '
+#                            'with discrete actions that apply a constant force on either the left ' 
+#                            'or right side of the cart.', '/CartPole/Discrete/', 'CartPole'),
+#     'CartPole continuous' : ('A simple continuous state-action MDP for the classical cart-pole system by Rich Sutton, '
+#                              'with actions that describe the continuous force applied to the cart.', '/CartPole/Continuous/', 'CartPole'),
+#     'Elevators' : ('The Elevator domain models evening rush hours when people from different floors in '
+#                    'a building want to go down to the bottom floor using elevators', '/Elevator/', 'Elevator'),
+#     'MarsRover' : ('Multi Rover Navigation, where a group of agent needs to harvest mineral.', '/Mars_rover/', 'MarsRover'),
+#     'MountainCar' : ('A simple continuous MDP for the classical mountain car control problem.', '/MountainCar/', 'MountainCar'),
+#     'PowerGeneration' : ('A simple power generation problem loosely modeled on the problem of unit commitment.',
+#                           '/Power_gen/', 'PowerGen'),
+#     'RaceCar' : ('A simple continuous MDP for the racecar problem.', '/Racecar/', 'Racecar'),
+#     'RecSim' : ('A problem of recommendation systems, with consumers and providers.', '/Recsim/', 'RecSim'),
+#     'UAV continuous' : ('Continous action space version of multi-UAV problem where a group of UAVs have to reach goal '
+#                         'positions in  in the 3d Space.', '/UAV/Continuous/', 'UAVs'),
+#     'UAV discrete' : ('Discrete action space version of multi-UAV problem where a group of UAVs have to reach goal '
+#     'positions in  in the 3d Space.', '/UAV/Discrete/', 'UAVs'),
+#     'UAV mixed' : ('Mixed action space version of multi-UAV problem where a group of UAVs have to reach goal '
+#     'positions in  in the 3d Space.', '/UAV/Mixed/', 'UAVs'),
+#     'Wildfire' : ('A boolean version of the wildfire fighting domain.', '/Wildfire/', 'Wildfire'),
+#     'SupplyChain' : ('A supply chain with factory and multiple warehouses.', '/Supply_Chain/', 'SupplyChain'),
+#     'Traffic' : ('BLX/QTM traffic model.', '/Traffic/', None),
+#     'PropDBN' : ('Simple propositional DBN.', '/PropDBN/', None),
+#     'WildlifePreserve' : ('Complex domain from IPPC 2018.', '/WildlifePreserve/', None),
+#     'NewLanguage' : ('Example with new language features.', '/NewLanguageExamples/NewLanguage/', None),
+#     'NewtonZero' : ('Example with Newton root-finding method.', '/NewLanguageExamples/NewtonZero/', None),
+#     'Reservoir': ('Managing the water level in interconnected reservoirs.', '/Reservoir/', None)
+# }
 
+EXP_DICT = Manifest.load()
 
 # class RDDLEnvironmentNotExist(ValueError):
 #     pass
@@ -42,11 +44,12 @@ class RDDLInstanceNotExist(ValueError):
 
 
 class ExampleManager:
+
     def __init__(self, env: str):
         self.env = env
         if env not in EXP_DICT:
-            raise RDDLEnvironmentNotExist("Environment {} does not exist".format(env)
-                          + ExampleManager._print_stack_trace(env))
+            raise RDDLEnvironmentNotExist("Environment {} does not exist".format(env) +
+                                          ExampleManager._print_stack_trace(env))
 
         self.path_to_env = os.path.dirname(os.path.abspath(__file__)) + EXP_DICT[env][1]
 
@@ -64,7 +67,7 @@ class ExampleManager:
         return instances
 
     def get_instance(self, num: int):
-        instance = 'instance' + str(num)+ '.rddl'
+        instance = 'instance' + str(num) + '.rddl'
         if not os.path.exists(self.path_to_env + instance):
             raise RDDLInstanceNotExist(
                 "instance {} does not exist for example environment {}".format(instance, self.env) +
