01219e0be719cfc6d0f4407bb9864fb783694228
Mike Gimelfarb <35513382+mike-gimelfarb@users.noreply.github.com>
2023-02-07 19:11:40 -0500

    Minor fixes to ground name and parse name RDDLModel API

--- pyRDDLGym/Core/Compiler/RDDLModel.py
+++ pyRDDLGym/Core/Compiler/RDDLModel.py
@@ -346,31 +346,33 @@ class PlanningModel(metaclass=ABCMeta):
         '''Given a variable name and list of objects as arguments, produces a 
         grounded representation <variable>___<obj1>__<obj2>__...
         '''
-        is_primed = name.endswith(PlanningModel.NEXT_STATE_SYM)
+        PRIME = PlanningModel.NEXT_STATE_SYM
+        is_primed = name.endswith(PRIME)
         var = name
         if is_primed:
-            var = var[:-1]
+            var = var[:-len(PRIME)]
         if objects is not None and objects:
             objects = PlanningModel.OBJECT_SEP.join(objects)
             var += PlanningModel.FLUENT_SEP + objects
         if is_primed:
-            var += PlanningModel.NEXT_STATE_SYM
+            var += PRIME
         return var
     
     def parse(self, expr: str) -> Tuple[str, List[str]]:
         '''Parses an expression of the form <name> or <name>___<type1>__<type2>...)
         into a tuple of <name>, [<type1>, <type2>, ...].
         '''
-        is_primed = expr.endswith(PlanningModel.NEXT_STATE_SYM)
+        PRIME = PlanningModel.NEXT_STATE_SYM
+        is_primed = expr.endswith(PRIME)
         if is_primed:
-            expr = expr[:-1]
+            expr = expr[:-len(PRIME)]
         var, *objects = expr.split(PlanningModel.FLUENT_SEP)
         if objects:
             if len(objects) != 1:
                 raise RDDLInvalidObjectError(f'Invalid pvariable expression {expr}.')
             objects = objects[0].split(PlanningModel.OBJECT_SEP)
         if is_primed:
-            var += PlanningModel.NEXT_STATE_SYM
+            var += PRIME
         return var, objects
     
     def variations(self, ptypes: Iterable[str]) -> Iterable[Tuple[str, ...]]:
