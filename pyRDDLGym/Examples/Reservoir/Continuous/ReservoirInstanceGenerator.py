507d128d441b5a374d9c679929466403bc4eace1
Mike Gimelfarb <35513382+mike-gimelfarb@users.noreply.github.com>
2023-03-11 23:20:38 -0500

    Update ReservoirInstanceGenerator.py

--- pyRDDLGym/Examples/Reservoir/Continuous/ReservoirInstanceGenerator.py
+++ pyRDDLGym/Examples/Reservoir/Continuous/ReservoirInstanceGenerator.py
@@ -1,10 +1,12 @@
+import os
+
 from pyRDDLGym.Examples.InstanceGenerator import InstanceGenerator
 
 
 class ReservoirInstanceGenerator(InstanceGenerator):
     
     def get_env_name(self) -> str:
-        return 'Reservoir'
+        return os.path.join('Reservoir', 'Continuous')
     
     def generate_instance(self, instance: int) -> str:
         
@@ -12,19 +14,28 @@ class ReservoirInstanceGenerator(InstanceGenerator):
         if instance == 1:
             nodes, edges = self.generate_dag([(1, 2)])
             rlevels = [50., 60.]
-            horizon = 120
+            MIN_LEVEL = [20.0] * len(nodes)
+            MAX_LEVEL = [80.0] * len(nodes)
+            TOP_RES = [100.0] * len(nodes)
+            horizon = 100
             
         # three reservoirs
         elif instance == 2:
             nodes, edges = self.generate_dag([(1, 3), (2, 3)])
             rlevels = [45., 50., 50.]
-            horizon = 120
+            MIN_LEVEL = [20.0] * len(nodes)
+            MAX_LEVEL = [80.0] * len(nodes)
+            TOP_RES = [100.0] * len(nodes)
+            horizon = 100
         
         # five reservoirs
         elif instance == 3:
             nodes, edges = self.generate_dag([(1, 3), (2, 3), (3, 5), (4, 5)])
             rlevels = [45., 50., 50., 45., 60.]
-            horizon = 120
+            MIN_LEVEL = [20.0] * len(nodes)
+            MAX_LEVEL = [80.0] * len(nodes)
+            TOP_RES = [100.0] * len(nodes)
+            horizon = 100
         
         # ten reservoirs
         elif instance == 4:
@@ -32,7 +43,10 @@ class ReservoirInstanceGenerator(InstanceGenerator):
                                               (3, 7), (4, 7), (5, 8), (6, 8), 
                                               (6, 9), (7, 9), (8, 10), (9, 10)])
             rlevels = [45., 50., 50., 60., 50., 50., 50., 40., 50., 95.]
-            horizon = 120
+            MIN_LEVEL = [20.0] * len(nodes)
+            MAX_LEVEL = [80.0] * len(nodes)
+            TOP_RES = [100.0] * len(nodes)
+            horizon = 100
         
         # twenty reservoirs
         elif instance == 5:
@@ -41,8 +55,11 @@ class ReservoirInstanceGenerator(InstanceGenerator):
                                               (9, 10), (10, 11), (11, 12), (12, 13),
                                               (13, 14), (14, 15), (15, 16), (16, 17),
                                               (17, 18), (18, 19), (19, 20)])
-            rlevels = [75.] * 20
-            horizon = 120
+            MIN_LEVEL = [60.0] * len(nodes)
+            MAX_LEVEL = [140.0] * len(nodes)
+            TOP_RES = [200.0] * len(nodes)
+            rlevels = [80.] * 20
+            horizon = 100
             
         else:
             raise Exception(f'Invalid instance {instance} for Reservoir.')
@@ -57,6 +74,12 @@ class ReservoirInstanceGenerator(InstanceGenerator):
         for (e1, e2) in edges:
             nfs.append(f'RES_CONNECT({e1},{e2});')
         nfs.append(f'CONNECTED_TO_SEA({nodes[-1]});')    
+        for (node, minl) in zip(nodes, MIN_LEVEL):
+            nfs.append(f'MIN_LEVEL({node}) = {minl};')
+        for (node, maxl) in zip(nodes, MAX_LEVEL):
+            nfs.append(f'MAX_LEVEL({node}) = {maxl};')
+        for (node, topl) in zip(nodes, TOP_RES):
+            nfs.append(f'TOP_RES({node}) = {topl};')
         value += '\n\t'.join(nfs)
         value += '\n\t' + '};'
         value += '\n' + '}'
@@ -81,5 +104,5 @@ class ReservoirInstanceGenerator(InstanceGenerator):
         return nodes, edges
 
 inst = ReservoirInstanceGenerator()
-print(inst.generate_instance(5))
+inst.save_instance(5)
         
