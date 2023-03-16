4b6c2655111291c1eb7adfeb555bc0da4a951207
Mike Gimelfarb <35513382+mike-gimelfarb@users.noreply.github.com>
2023-03-15 21:02:54 -0400

    Rewrote reservoir instance gen

--- pyRDDLGym/Examples/InstanceGenerators/ReservoirInstanceGenerator.py
+++ pyRDDLGym/Examples/InstanceGenerators/ReservoirInstanceGenerator.py
@@ -1,4 +1,5 @@
 import os
+import random
 from typing import Dict
 
 from pyRDDLGym.Examples.InstanceGenerator import InstanceGenerator
@@ -10,81 +11,84 @@ class ReservoirInstanceGenerator(InstanceGenerator):
         return os.path.join('Reservoir', 'Continuous')
     
     def get_domain_name(self) -> str:
-        return 'reservoir_control_dis'
+        return 'reservoir_control_cont'
     
-    def generate_rddl_variables(self, params: Dict[str, object]) -> Dict[str, object]: 
-        nodes, edges = self.generate_dag(params['connected'])
-        reservoirs = [f't{i + 1}' for i in range(len(nodes))]
+    def _generate_random_dag(self, n, maxedges):
+        nodes = list(range(n))
+        random.shuffle(nodes)
+        graph = {node: set() for node in nodes}
+        for i in range(n - 2, -1, -1):
+            upstream = nodes[i + 1:]
+            numedges = random.randint(1, min(maxedges, len(upstream)))
+            children = random.sample(upstream, numedges)
+            graph[nodes[i]].update(children)
+        connected_to_sea = nodes[-1]
+        return graph, connected_to_sea
+
+    def sample_instance(self, params: Dict[str, object]) -> Dict[str, object]:
+        n = params['num_reservoirs']
+        graph, connected_to_sea = self._generate_random_dag(n, params['max_edges'])
+        
+        reservoirs = [f't{i + 1}' for i in range(n)]
         
         nonfluents = {}
-        for (e1, e2) in edges:
-            nonfluents[f'RES_CONNECT({e1}, {e2})'] = True
-        nonfluents[f'CONNECTED_TO_SEA({nodes[-1]})'] = True
-        if 'MIN_LEVEL' in params:
-            for (res, val) in zip(reservoirs, params['MIN_LEVEL']):
-                nonfluents[f'MIN_LEVEL({res})'] = val
-        if 'MAX_LEVEL' in params:
-            for (res, val) in zip(reservoirs, params['MAX_LEVEL']):
-                nonfluents[f'MAX_LEVEL({res})'] = val
-        if 'TOP_RES' in params:
-            for (res, val) in zip(reservoirs, params['TOP_RES']):
-                nonfluents[f'TOP_RES({res})'] = val
+        for t1, out in graph.items():
+            for t2 in out:
+                nonfluents[f'RES_CONNECT({reservoirs[t1]}, {reservoirs[t2]})'] = True
+        nonfluents[f'CONNECTED_TO_SEA({reservoirs[connected_to_sea]})'] = True
         
         states = {}
-        if 'rlevel' in params:
-            for (res, val) in zip(reservoirs, params['rlevel']):
-                states[f'rlevel({res})'] = val
+        for i, res in enumerate(reservoirs):
+            top = random.uniform(*params['top_range'])
+            range_prop = random.uniform(*params['target_range'])
+            range_top = range_prop * top
+            lower = random.uniform(0., top - range_top)
+            upper = lower + range_top
+            nonfluents[f'MIN_LEVEL({res})'] = lower
+            nonfluents[f'MAX_LEVEL({res})'] = upper
+            nonfluents[f'TOP_RES({res})'] = top
+            nonfluents[f'RAIN_VAR({res})'] = params['rain_var']
+            states[f'rlevel({res})'] = random.uniform(0., top * 0.9)
         
         return {
             'objects': {'reservoir': reservoirs},
             'non-fluents': nonfluents,
             'init-states': states,
-            'horizon': 100,
-            'discount': 1.0,
+            'horizon': params['horizon'],
+            'discount': params['discount'],
             'max-nondef-actions': 'pos-inf'
         }
             
-    def generate_dag(self, edges):
-        nodes = sorted({i for tup in edges for i in tup})
-        nodes = [f't{node}' for node in nodes]
-        edges = [(f't{e1}', f't{e2}') for (e1, e2) in edges]
-        return nodes, edges
-
     
 params = [
     
-    # two reservoirs
-    {'connected': [(1, 2)],
-     'MIN_LEVEL': [20.] * 2, 'MAX_LEVEL': [80.] * 2, 'TOP_RES': [100.] * 2,
-     'rlevel': [50., 60.]},
+    # 2 reservoirs
+    {'num_reservoirs': 2, 'max_edges': 1,
+     'top_range': (100., 200.), 'target_range': (0.4, 0.8), 'rain_var': 5.,
+     'horizon': 100, 'discount': 1.0},
     
-    # three reservoirs
-    {'connected': [(1, 3), (2, 3)],
-     'MIN_LEVEL': [20.] * 3, 'MAX_LEVEL': [80.] * 3, 'TOP_RES': [100.] * 3,
-     'rlevel': [45., 50., 50.]},
+    # 5 reservoirs
+    {'num_reservoirs': 5, 'max_edges': 2,
+     'top_range': (200., 400.), 'target_range': (0.3, 0.7), 'rain_var': 8.,
+     'horizon': 100, 'discount': 1.0},
     
-    # five reservoirs
-    {'connected': [(1, 3), (2, 3), (3, 5), (4, 5)],
-     'MIN_LEVEL': [20.] * 5, 'MAX_LEVEL': [80.] * 5, 'TOP_RES': [100.] * 5,
-     'rlevel': [45., 50., 50., 45., 60.]},
+    # 10 reservoirs
+    {'num_reservoirs': 10, 'max_edges': 4,
+     'top_range': (300., 500.), 'target_range': (0.3, 0.6), 'rain_var': 10.,
+     'horizon': 100, 'discount': 1.0},
     
-    # ten reservoirs
-    {'connected': [(1, 5), (2, 5), (2, 6), (3, 6),
-                   (3, 7), (4, 7), (5, 8), (6, 8),
-                   (6, 9), (7, 9), (8, 10), (9, 10)],
-     'MIN_LEVEL': [20.] * 10, 'MAX_LEVEL': [80.] * 10, 'TOP_RES': [100.] * 10,
-     'rlevel': [45., 50., 50., 60., 50., 50., 50., 40., 50., 95.]},
+    # 25 reservoirs
+    {'num_reservoirs': 25, 'max_edges': 10,
+     'top_range': (400., 600.), 'target_range': (0.2, 0.5), 'rain_var': 20.,
+     'horizon': 100, 'discount': 1.0},
     
-    # twenty reservoirs
-    {'connected': [(1, 2), (2, 3), (3, 4), (4, 5),
-                   (5, 6), (6, 7), (7, 8), (8, 9),
-                   (9, 10), (10, 11), (11, 12), (12, 13),
-                   (13, 14), (14, 15), (15, 16), (16, 17),
-                   (17, 18), (18, 19), (19, 20)],
-     'MIN_LEVEL': [70.] * 20, 'MAX_LEVEL': [140.] * 20, 'TOP_RES': [200.] * 20,
-     'rlevel': [80.] * 20},
+    # 50 reservoirs
+    {'num_reservoirs': 50, 'max_edges': 30,
+     'top_range': (500., 700.), 'target_range': (0.1, 0.3), 'rain_var': 30.,
+     'horizon': 100, 'discount': 1.0}
 ]
 
 inst = ReservoirInstanceGenerator()
-inst.save_instances(params)
+for i, param in enumerate(params):
+    inst.save_instance(i + 1, param)
         
