1d599b3feb23bae9db5956ee624d2cb6c10e6fa7
Mike Gimelfarb <35513382+mike-gimelfarb@users.noreply.github.com>
2023-03-17 22:43:30 -0400

    Added logging of trials to jax example

--- pyRDDLGym/JaxExample.py
+++ pyRDDLGym/JaxExample.py
@@ -1,13 +1,15 @@
 import jax
+import numpy as np
 import sys
 import time
-
 
 from pyRDDLGym.Planner import JaxConfigManager
 from pyRDDLGym.Core.Compiler.RDDLDecompiler import RDDLDecompiler
 # from pyRDDLGym.Core.Jax.JaxRDDLModelError import JaxRDDLModelError
 
-timeout = 60 * 5
+timeout = 60 * 3
+trials = 1
+save = False
 
 
 def print_parameterized_exprs(planner):
@@ -49,67 +51,84 @@ def slp_train(planner, budget, **train_args):
 
 
 def slp_no_replan(env):
-    myEnv, planner, train_args = JaxConfigManager.get(f'{env}.cfg')
-    params = slp_train(planner, timeout, **train_args)
-    
+    myEnv, planner, train_args, (dom, inst) = JaxConfigManager.get(f'{env}.cfg')
     key = train_args['key']    
-    total_reward = 0
-    state = myEnv.reset()
-    for step in range(myEnv.horizon):
-        subs = myEnv.sampler.subs
-        key, subkey = jax.random.split(key)
-        action = planner.get_action(subkey, params, step, subs)
-        next_state, reward, done, _ = myEnv.step(action)
-        total_reward += reward 
-        print()
-        print('step       = {}'.format(step))
-        print('state      = {}'.format(state))
-        print('action     = {}'.format(action))
-        print('next state = {}'.format(next_state))
-        print('reward     = {}'.format(reward))
-        state = next_state
-        if done:
-            break
-    print(f'episode ended with reward {total_reward}')
+    
+    rewards = np.zeros((myEnv.horizon, trials))
+    for trial in range(trials):
+        print('\n' + '*' * 30 + '\n' + f'starting trial {trial + 1}\n' + '*' * 30)
+        train_args['key'] = key
+        params = slp_train(planner, timeout, **train_args)
+        
+        total_reward = 0
+        state = myEnv.reset()
+        for step in range(myEnv.horizon):
+            subs = myEnv.sampler.subs
+            key, subkey = jax.random.split(key)
+            action = planner.get_action(subkey, params, step, subs)
+            next_state, reward, done, _ = myEnv.step(action)
+            total_reward += reward 
+            rewards[step, trial] = reward
+            
+            print()
+            print('step       = {}'.format(step))
+            print('state      = {}'.format(state))
+            print('action     = {}'.format(action))
+            print('next state = {}'.format(next_state))
+            print('reward     = {}'.format(reward))
+            state = next_state
+            if done:
+                break
+        print(f'episode ended with reward {total_reward}')
+        
     myEnv.close()
+    if save:
+        np.savetxt(f'{dom}_{inst}_slp.csv', rewards, delimiter=',')
 
     
 def slp_replan(env):
-    myEnv, planner, train_args = JaxConfigManager.get(f'{env}.cfg')
-    
+    myEnv, planner, train_args, (dom, inst) = JaxConfigManager.get(f'{env}.cfg')
     key = train_args['key']
-    total_reward = 0
-    state = myEnv.reset() 
-    starttime = time.time()
-    for step in range(myEnv.horizon):
-        currtime = time.time()
-        elapsed = currtime - starttime
+    
+    rewards = np.zeros((myEnv.horizon, trials))
+    for trial in range(trials):
+        print('\n' + '*' * 30 + '\n' + f'starting trial {trial + 1}\n' + '*' * 30)
+        total_reward = 0
+        state = myEnv.reset() 
+        starttime = time.time()
+        for step in range(myEnv.horizon):
+            currtime = time.time()
+            elapsed = currtime - starttime
+            
+            if elapsed < timeout:
+                subs = myEnv.sampler.subs
+                params = slp_train(planner, budget=timeout - elapsed,
+                                   subs=subs, **train_args)
+                key, subkey = jax.random.split(key)
+                action = planner.get_action(subkey, params, 0, subs)
+            else:
+                print('ran out of time!')
+                action = {}
+            
+            next_state, reward, done, _ = myEnv.step(action)
+            total_reward += reward 
+            rewards[step, trial] = reward
+            
+            print()
+            print(f'elapsed    = {elapsed} s')
+            print(f'step       = {step}')
+            print(f'state      = {state}')
+            print(f'action     = {action}')
+            print(f'next state = {next_state}')
+            print(f'reward     = {reward}')
+            state = next_state
+            if done: 
+                break
+        print(f'episode ended with reward {total_reward}')
         
-        if elapsed < timeout:
-            subs = myEnv.sampler.subs
-            params = slp_train(planner, budget=timeout - elapsed, 
-                               subs=subs, **train_args)
-            key, subkey = jax.random.split(key)
-            action = planner.get_action(subkey, params, 0, subs)
-        else:
-            print('ran out of time!')
-            action = {}
-        
-        next_state, reward, done, _ = myEnv.step(action)
-        total_reward += reward 
-        
-        print()
-        print(f'elapsed    = {elapsed} s')
-        print(f'step       = {step}')
-        print(f'state      = {state}')
-        print(f'action     = {action}')
-        print(f'next state = {next_state}')
-        print(f'reward     = {reward}')
-        state = next_state
-        if done: 
-            break
-    print(f'episode ended with reward {total_reward}')
     myEnv.close()
+    if save:
+        np.savetxt(f'{dom}_{inst}_mpc.csv', rewards, delimiter=',')
 
     
 def main(env, replan):
