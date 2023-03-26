ee4841371d75af4c61079f1246f00a05aa7942f1
Mike Gimelfarb <35513382+mike-gimelfarb@users.noreply.github.com>
2023-03-26 19:12:56 -0400

    Fixes in jax parameter tuning

--- pyRDDLGym/Core/Jax/JaxParameterTuning.py
+++ pyRDDLGym/Core/Jax/JaxParameterTuning.py
@@ -3,23 +3,37 @@ import jax
 import numpy as np
 import optax
 import time
+from typing import Dict, List, Tuple, Union
 
 from pyRDDLGym import ExampleManager
 from pyRDDLGym import RDDLEnv
 from pyRDDLGym.Core.Jax.JaxRDDLLogic import FuzzyLogic
 from pyRDDLGym.Core.Jax.JaxRDDLBackpropPlanner import JaxRDDLBackpropPlanner
 from pyRDDLGym.Core.Jax.JaxRDDLBackpropPlanner import JaxStraightLinePlan
+np.seterr(all='warn')
 
 
 class JaxParameterTuning:
     '''A general-purpose class for tuning a Jax planner.'''
     
-    def __init__(self, env, action_bounds,
-                 max_train_epochs, timeout_episode, timeout_epoch,
-                 gp_iterations, initial_stddevs, learning_rates, model_weights,
-                 planning_horizons, eval_horizon=None,
-                 init_num_points=1, batch_size_train=32, print_step=None,
-                 verbose=False) -> None:
+    def __init__(self, env: RDDLEnv,
+                 action_bounds: Dict[str, Tuple[float, float]],
+                 max_train_epochs: int,
+                 timeout_episode: float,
+                 timeout_epoch: float,
+                 gp_iterations: int,
+                 initial_stddevs: Union[Tuple[float, float], List[float]],
+                 learning_rates: Union[Tuple[float, float], List[float]],
+                 model_weights: Union[Tuple[float, float], List[float]],
+                 planning_horizons: Union[Tuple[float, float], List[float]],
+                 key: int=42,
+                 eval_horizon: int=None,
+                 init_num_points: int=1,
+                 batch_size_train: int=32,
+                 print_step: int=None,
+                 verbose: bool=True,
+                 wrap_sigmoid: bool=True,
+                 **planner_kwargs) -> None:
         '''Creates a new instance for tuning hyper-parameters for Jax planners
         on the given RDDL domain and instance.
         
@@ -42,19 +56,23 @@ class JaxParameterTuning:
         relaxation to tune
         :param planning_horizons: set of possible lookahead horizons for MPC to
         tune
+        :param key: seed for JAX PRNG key
         :param eval_horizon: maximum number of decision epochs to evaluate (also
         applies for training if using straight-line planning)
         :param init_num_points: number of iterations to seed Bayesian optimizer
         (these are used as initial guess to build loss surrogate model)
         :param batch_size_train: training batch size for planner
         :param print_step: how often to print training callback
-        :parma verbose: whether to print intermediate results of tuning
+        :param verbose: whether to print intermediate results of tuning
+        :param wrap_sigmoid: whether to wrap bool action-fluents with sigmoid
+        :param planner_kwargs: additional arguments to feed to the planner
         '''
         self.env = env
         self.action_bounds = action_bounds
         self.max_train_epochs = max_train_epochs
         self.timeout_episode = timeout_episode
         self.timeout_epoch = timeout_epoch
+        self.key = key
         self.evaluation_horizon = eval_horizon
         if eval_horizon is None:
             self.evaluation_horizon = env.horizon
@@ -63,6 +81,8 @@ class JaxParameterTuning:
         self.batch_size_train = batch_size_train
         self.print_step = print_step
         self.verbose = verbose
+        self.wrap_sigmoid = wrap_sigmoid
+        self.planner_kwargs = planner_kwargs
         
         # set up bounds for parameter search
         self.bounds = self._get_bounds(
@@ -103,10 +123,30 @@ class JaxParameterTuning:
                     str(callback['iteration']).rjust(4),
                     callback['train_return'],
                     callback['test_return']))
+            if not np.isfinite(callback['train_return']):
+                if self.verbose:
+                    print('aborting due to NaN or inf value')
+                break
             if elapsed >= timeout:
                 break
         return callback
     
+    def _bayes_optimize(self, objective, name):
+        myBopt = GPyOpt.methods.BayesianOptimization(
+            f=objective,
+            domain=self.bounds,
+            initial_design_numdata=self.init_num_points,
+            maximize=True)
+        myBopt.run_optimization(
+            self.gp_iterations,
+            verbosity=self.verbose,
+            save_models_parameters=True,
+            report_file=f'{name}_report.txt',
+            evaluations_file=f'{name}_eval.txt',
+            models_file=f'{name}_models.txt')
+        myBopt.plot_acquisition(f'{name}_acq.pdf')
+        myBopt.plot_convergence(f'{name}_conv.pdf')
+
     def tune_slp(self):
         '''Tunes the hyper-parameters for Jax planner using straight-line 
         planning approach.'''
@@ -120,41 +160,30 @@ class JaxParameterTuning:
                 print('\n' + f'optimizing SLP with std={std}, lr={lr}, w={w}...')
             
             # initialize planner
-            key = jax.random.PRNGKey(42)
+            key = jax.random.PRNGKey(self.key)
             planner = JaxRDDLBackpropPlanner(
                 rddl=env.model,
                 plan=JaxStraightLinePlan(
-                    initializer=jax.nn.initializers.normal(std)),
+                    initializer=jax.nn.initializers.normal(std),
+                    wrap_sigmoid=self.wrap_sigmoid),
                 batch_size_train=self.batch_size_train,
                 rollout_horizon=self.evaluation_horizon,
                 action_bounds=self.action_bounds,
                 optimizer=optax.rmsprop(lr),
-                logic=FuzzyLogic(weight=w))
+                logic=FuzzyLogic(weight=w),
+                **self.planner_kwargs)
             policy_hyperparams = {name: w for name in self.action_bounds}
-            np.seterr(all='warn')
             
             # perform training
             callback = self._train_epoch(
                 key, policy_hyperparams, None, planner, timeout_episode)
-            total_reward = callback['test_return']
+            total_reward = callback['best_return']
             if self.verbose:
                 print(f'total reward={total_reward}\n')
             return total_reward
         
         # run Bayesian optimization
-        myBopt = GPyOpt.methods.BayesianOptimization(
-            f=objective,
-            domain=self.bounds,
-            initial_design_numdata=self.init_num_points,
-            maximize=True)
-        myBopt.run_optimization(self.gp_iterations,
-                                verbosity=self.verbose,
-                                save_models_parameters=True,
-                                report_file='gp_slp_report.txt',
-                                evaluations_file='gp_slp_eval.txt',
-                                models_file='gp_slp_models.txt')
-        myBopt.plot_acquisition('gp_slp_acq.pdf')
-        myBopt.plot_convergence('gp_slp_conv.pdf')
+        self._bayes_optimize(objective, 'gp_slp')
         
     def tune_mpc(self):
         '''Tunes the hyper-parameters for Jax planner using MPC/receding horizon
@@ -171,18 +200,19 @@ class JaxParameterTuning:
                 print('\n' + f'optimizing MPC with std={std}, lr={lr}, w={w}, T={T}...')
             
             # initialize planner
-            key = jax.random.PRNGKey(42)
+            key = jax.random.PRNGKey(self.key)
             planner = JaxRDDLBackpropPlanner(
                 rddl=env.model,
                 plan=JaxStraightLinePlan(
-                    initializer=jax.nn.initializers.normal(std)),
+                    initializer=jax.nn.initializers.normal(std),
+                    wrap_sigmoid=self.wrap_sigmoid),
                 batch_size_train=self.batch_size_train,
                 rollout_horizon=T,
                 action_bounds=self.action_bounds,
                 optimizer=optax.rmsprop(lr),
-                logic=FuzzyLogic(weight=w))
+                logic=FuzzyLogic(weight=w),
+                **self.planner_kwargs)
             policy_hyperparams = {name: w for name in self.action_bounds}
-            np.seterr(all='warn')
             
             # perform training and collect rewards
             total_reward = 0.0
@@ -212,37 +242,26 @@ class JaxParameterTuning:
             return total_reward
         
         # run Bayesian optimization
-        myBopt = GPyOpt.methods.BayesianOptimization(
-            f=objective,
-            domain=self.bounds,
-            initial_design_numdata=self.init_num_points,
-            maximize=True)
-        myBopt.run_optimization(self.gp_iterations,
-                                verbosity=self.verbose,
-                                save_models_parameters=True,
-                                report_file='gp_mpc_report.txt',
-                                evaluations_file='gp_mpc_eval.txt',
-                                models_file='gp_mpc_models.txt')
-        myBopt.plot_acquisition('gp_mpc_acq.pdf')
-        myBopt.plot_convergence('gp_mpc_conv.pdf')
+        self._bayes_optimize(objective, 'gp_mpc')
 
 
 if __name__ == '__main__':
-    EnvInfo = ExampleManager.GetEnvInfo('HVAC')
-    env = RDDLEnv.RDDLEnv(EnvInfo.get_domain(), EnvInfo.get_instance(1))
+    EnvInfo = ExampleManager.GetEnvInfo('Traffic')
+    env = RDDLEnv.RDDLEnv(EnvInfo.get_domain(), EnvInfo.get_instance(0))
     tuning = JaxParameterTuning(
         env=env,
-        action_bounds={'fan-in': (0.05, None), 'heat-input': (0.0, None)},
-        max_train_epochs=10000,
-        timeout_episode=60,
+        action_bounds={'advance': (0., 1.)},
+        max_train_epochs=1000,
+        timeout_episode=300,
         timeout_epoch=None,
         gp_iterations=20,
         initial_stddevs=(0., 1.),
         learning_rates=[1e-5, 5e-5, 1e-4, 5e-4, 1e-3, 5e-3, 1e-2, 5e-2, 1e-1, 5e-1],
         model_weights=(0., 200.),
         planning_horizons=None,
+        eval_horizon=300,
         batch_size_train=8,
-        eval_horizon=40,
-        print_step=500,
-        verbose=True)
+        print_step=100,
+        use64bit=True,
+        clip_grad=1.0)
     tuning.tune_slp()
