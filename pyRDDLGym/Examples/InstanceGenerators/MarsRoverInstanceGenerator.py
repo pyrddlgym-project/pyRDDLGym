5b69be0b84d696b0ee7f1fb0b7c0bd60f6ae1f33
Mike Gimelfarb <35513382+mike-gimelfarb@users.noreply.github.com>
2023-04-04 23:17:51 -0400

    Fixed bug in mars rover instance generator

--- pyRDDLGym/Examples/InstanceGenerators/MarsRoverInstanceGenerator.py
+++ pyRDDLGym/Examples/InstanceGenerators/MarsRoverInstanceGenerator.py
@@ -11,7 +11,7 @@ class MarsRoverInstanceGenerator(InstanceGenerator):
     
     def get_domain_name(self) -> str:
         return 'mars_rover_science_mission'
-    
+            
     def sample_instance(self, params: Dict[str, object]) -> Dict[str, object]:
         nm = params['num_minerals']
         nr = params['num_rovers']
@@ -26,7 +26,7 @@ class MarsRoverInstanceGenerator(InstanceGenerator):
         nonfluents = {}
         for m in minerals:
             nonfluents[f'MINERAL-POS-X({m})'] = random.uniform(x1, x2)
-            nonfluents[f'MINERAL-POS-X({m})'] = random.uniform(y1, y2)
+            nonfluents[f'MINERAL-POS-Y({m})'] = random.uniform(y1, y2)
             nonfluents[f'MINERAL-AREA({m})'] = random.uniform(a1, a2)
             nonfluents[f'MINERAL-VALUE({m})'] = random.uniform(v1, v2)
         
@@ -45,7 +45,7 @@ class MarsRoverInstanceGenerator(InstanceGenerator):
             'discount': params['discount'],
             'max-nondef-actions': 'pos-inf'
         }
-            
+    
     
 params = [
     
@@ -54,27 +54,29 @@ params = [
     # others, the agent is forced to delay some small immediate reward for taking
     # some additional time steps and fuel to accept a much larger reward in future.
     {'num_minerals': 2, 'num_rovers': 4, 'location_bounds': (-10., 10.),
-     'area_bounds': (5., 8.), 'value_bounds': (0., 20.), 
+     'area_bounds': (3., 6.), 'value_bounds': (0., 20.), 
      'horizon': 100, 'discount': 1.0},
     
     {'num_minerals': 5, 'num_rovers': 6, 'location_bounds': (-10., 10.),
-     'area_bounds': (5., 8.), 'value_bounds': (0., 20.), 
+     'area_bounds': (2., 6.), 'value_bounds': (0., 20.), 
      'horizon': 100, 'discount': 1.0},
     
     {'num_minerals': 8, 'num_rovers': 8, 'location_bounds': (-10., 10.),
-     'area_bounds': (4., 7.), 'value_bounds': (0., 20.), 
+     'area_bounds': (2., 5.), 'value_bounds': (0., 20.), 
      'horizon': 100, 'discount': 1.0},
     
-    {'num_minerals': 12, 'num_rovers': 8, 'location_bounds': (-10., 10.),
-     'area_bounds': (4., 7.), 'value_bounds': (0., 20.), 
+    {'num_minerals': 12, 'num_rovers': 9, 'location_bounds': (-10., 10.),
+     'area_bounds': (1., 4.), 'value_bounds': (0., 20.), 
      'horizon': 100, 'discount': 1.0},
     
     {'num_minerals': 20, 'num_rovers': 10, 'location_bounds': (-10., 10.),
-     'area_bounds': (3., 6.), 'value_bounds': (0., 20.), 
+     'area_bounds': (1., 3.), 'value_bounds': (0., 20.), 
      'horizon': 100, 'discount': 1.0}
 ]
+
 
 inst = MarsRoverInstanceGenerator()
 for i, param in enumerate(params):
     inst.save_instance(i + 1, param)
         
+
