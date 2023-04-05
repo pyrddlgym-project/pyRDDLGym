f2b7e787ff1a57d567672836ab75f3ab2c5142ed
Mike Gimelfarb <35513382+mike-gimelfarb@users.noreply.github.com>
2023-04-05 01:10:21 -0400

    Update ReservoirInstanceGenerator.py

--- pyRDDLGym/Examples/InstanceGenerators/ReservoirInstanceGenerator.py
+++ pyRDDLGym/Examples/InstanceGenerators/ReservoirInstanceGenerator.py
@@ -66,23 +66,23 @@ params = [
     # the rain variance and the narrowing of the target water level ranges all
     # contribute to difficulty
     {'num_reservoirs': 2, 'max_edges': 1,
-     'top_range': (100., 200.), 'target_range': (0.4, 0.8), 'rain_var': 5.,
+     'top_range': (100., 200.), 'target_range': (0.4, 0.7), 'rain_var': 5.,
      'horizon': 100, 'discount': 1.0},
     
     {'num_reservoirs': 5, 'max_edges': 2,
-     'top_range': (100., 300.), 'target_range': (0.3, 0.7), 'rain_var': 10.,
+     'top_range': (100., 300.), 'target_range': (0.3, 0.6), 'rain_var': 10.,
      'horizon': 100, 'discount': 1.0},
     
     {'num_reservoirs': 10, 'max_edges': 5,
-     'top_range': (100., 400.), 'target_range': (0.3, 0.6), 'rain_var': 20.,
+     'top_range': (100., 400.), 'target_range': (0.2, 0.5), 'rain_var': 20.,
      'horizon': 100, 'discount': 1.0},
     
     {'num_reservoirs': 25, 'max_edges': 15,
-     'top_range': (100., 500.), 'target_range': (0.2, 0.5), 'rain_var': 40.,
+     'top_range': (100., 500.), 'target_range': (0.1, 0.4), 'rain_var': 40.,
      'horizon': 100, 'discount': 1.0},
     
     {'num_reservoirs': 50, 'max_edges': 40,
-     'top_range': (100., 600.), 'target_range': (0.1, 0.3), 'rain_var': 60.,
+     'top_range': (100., 600.), 'target_range': (0.1, 0.2), 'rain_var': 60.,
      'horizon': 100, 'discount': 1.0}
 ]
 
