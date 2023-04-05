ebae5fcf6599bf1e36992ddedc0c8ba78a78a649
Mike Gimelfarb <35513382+mike-gimelfarb@users.noreply.github.com>
2023-04-05 01:10:04 -0400

    Update HVACInstanceGenerator.py

--- pyRDDLGym/Examples/InstanceGenerators/HVACInstanceGenerator.py
+++ pyRDDLGym/Examples/InstanceGenerators/HVACInstanceGenerator.py
@@ -21,6 +21,8 @@ class HVACInstanceGenerator(InstanceGenerator):
         obj_zones = [f'z{i + 1}' for i in range(nz)]
         
         nonfluents = {}
+        nonfluents['TEMP-ZONE-MIN'] = params['TEMP-ZONE-MIN']
+        nonfluents['TEMP-ZONE-MAX'] = params['TEMP-ZONE-MAX']
         for h, h2z in zip(obj_heaters, heaters):
             for z in h2z:
                 nonfluents[f'ADJ-HEATER({h}, {obj_zones[z]})'] = True
@@ -30,11 +32,9 @@ class HVACInstanceGenerator(InstanceGenerator):
         
         states = {}
         for i, z in enumerate(obj_zones):
-            states[f'temp-zone({z})'] = random.uniform(
-                params['temp-zone-min'], params['temp-zone-max'])
+            states[f'temp-zone({z})'] = random.uniform(*params['temp-zone-range-init'])
         for i, h in enumerate(obj_heaters):
-            states[f'temp-heater({h})'] = random.uniform(
-                params['temp-heater-min'], params['temp-heater-max'])
+            states[f'temp-heater({h})'] = random.uniform(*params['temp-heater-range-init'])
                 
         return {
             'objects': {'zone': obj_zones, 'heater': obj_heaters},
@@ -70,35 +70,30 @@ class HVACInstanceGenerator(InstanceGenerator):
     
 params = [
     
-    # 3 zones, 2 heaters
-    {'num_heaters': 2, 'num_zones': 3, 'density': 0.2,
-     'temp-zone-min': 0., 'temp-zone-max': 10., 
-     'temp-heater-min': 0., 'temp-heater-max': 10.,
-     'horizon': 120, 'discount': 1.0},
+    {'num_heaters': 3, 'num_zones': 2, 'density': 0.2,
+     'temp-zone-range-init': (0., 10.), 'temp-heater-range-init': (0., 10.),
+     'TEMP-ZONE-MIN': 5.0, 'TEMP-ZONE-MAX': 15.0, 
+     'horizon': 100, 'discount': 1.0},
     
-    # 10 zones, 10 heaters
-    {'num_heaters': 10, 'num_zones': 10, 'density': 0.2, 
-     'temp-zone-min': 0., 'temp-zone-max': 10., 
-     'temp-heater-min': 0., 'temp-heater-max': 10.,
-     'horizon': 120, 'discount': 1.0},
+    {'num_heaters': 5, 'num_zones': 5, 'density': 0.25, 
+     'temp-zone-range-init': (0., 10.), 'temp-heater-range-init': (0., 10.),
+     'TEMP-ZONE-MIN': 5.0, 'TEMP-ZONE-MAX': 15.0, 
+     'horizon': 100, 'discount': 1.0},
     
-    # 25 zones, 25 heaters
-    {'num_heaters': 30, 'num_zones': 25, 'density': 0.2, 
-     'temp-zone-min': 0., 'temp-zone-max': 10., 
-     'temp-heater-min': 0., 'temp-heater-max': 10.,
-     'horizon': 120, 'discount': 1.0},
+    {'num_heaters': 8, 'num_zones': 10, 'density': 0.3, 
+     'temp-zone-range-init': (0., 10.), 'temp-heater-range-init': (0., 10.),
+     'TEMP-ZONE-MIN': 10.0, 'TEMP-ZONE-MAX': 15.0, 
+     'horizon': 100, 'discount': 1.0},
     
-    # 50 zones, 50 heaters
-    {'num_heaters': 50, 'num_zones': 50, 'density': 0.2, 
-     'temp-zone-min': 0., 'temp-zone-max': 10., 
-     'temp-heater-min': 0., 'temp-heater-max': 10.,
-     'horizon': 120, 'discount': 1.0},
+    {'num_heaters': 15, 'num_zones': 20, 'density': 0.9, 
+     'temp-zone-range-init': (0., 10.), 'temp-heater-range-init': (0., 10.),
+     'TEMP-ZONE-MIN': 20.0, 'TEMP-ZONE-MAX': 26.0, 
+     'horizon': 100, 'discount': 1.0},
     
-    # 100 zones, 100 heater
-    {'num_heaters': 100, 'num_zones': 100, 'density': 0.2, 
-     'temp-zone-min': 0., 'temp-zone-max': 10., 
-     'temp-heater-min': 0., 'temp-heater-max': 10.,
-     'horizon': 120, 'discount': 1.0}
+    {'num_heaters': 20, 'num_zones': 40, 'density': 0.4, 
+     'temp-zone-range-init': (0., 10.), 'temp-heater-range-init': (0., 10.),
+     'TEMP-ZONE-MIN': 21.0, 'TEMP-ZONE-MAX': 25.0, 
+     'horizon': 100, 'discount': 1.0}
 ]
 
 inst = HVACInstanceGenerator()
