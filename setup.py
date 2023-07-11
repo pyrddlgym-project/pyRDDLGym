d8c87fbb139e7cf6d6daef08638bb491e9fad86d
ataitler <ataitler@gmail.com>
2023-07-11 11:20:37 +0200

    setup.py version

--- setup.py
+++ setup.py
@@ -14,39 +14,19 @@
 
 from setuptools import setup, find_packages
 
-# with open("README.md", "r", encoding="utf-8") as fh:
-#     long_description = fh.read()
 
 setup(
       name='pyRDDLGym',
-      version='1.0.57',
+      version='1.1.0',
       author="Ayal Taitler, Michael Gimelfarb, Scott Sanner, Jihwan Jeong, Sriram Gopalakrishnan, Martin Mladenov, jack liu",
       author_email="ataitler@gmail.com, mike.gimelfarb@mail.utoronto.ca, ssanner@mie.utoronto.ca, jhjeong@mie.utoronto.ca, sriram.gopalakrishnan@jpmchase.com, mmladenov@google.com, xiaotian.liu@mail.utoronto.ca",
       description="pyRDDLGym: RDDL automatic generation tool for OpenAI Gym",
-      # long_description=long_description,
       license="MIT License",
       url="https://github.com/ataitler/pyRDDLGym",
       packages=find_packages(),
       install_requires=['ply', 'pillow>=9.2.0', 'matplotlib>=3.5.0', 'numpy>=1.22', 'gym>=0.24.0', 'pygame'],
       python_requires=">=3.8",
       include_package_data=True,
-      # package_data={'': ['Examples/CartPole/Continuous/*.rddl',
-      #                    'Examples/CartPole/Discrete/*.rddl',
-      #                    'Examples/Elevator/*.rddl',
-      #                    'Examples/Mars_rover/*.rddl',
-      #                    'Examples/MountainCar/*.rddl',
-      #                    'Examples/Power_gen/*.rddl',
-      #                    'Examples/Racecar/*.rddl',
-      #                    'Examples/Recsim/*.rddl',
-      #                    'Examples/UAV/Continuous/*.rddl',
-      #                    'Examples/UAV/Discrete/*.rddl',
-      #                    'Examples/UAV/Mixed/*.rddl',
-      #                    'Examples/Wildfire/*.rddl',
-      #                    'Examples/Supply_Chain/*.rddl',
-      #                    'Examples/Traffic/*.rddl',
-      #                    'Examples/PropDBN/*.rddl',
-      #                    'Examples/WildlifePreserve/*.rddl',
-      #                    ]},
       classifiers=[
         "Development Status :: 3 - Alpha",
         "Intended Audience :: Science/Research",
