27cc499e0fe64d8572bc479950f37f5000265514
Ilia Smirnov <iliathesmirnov@gmail.com>
2023-08-01 19:01:06 -0400

    Some minor clean-up. Sanity check method that "trains" using random samples

--- pyRDDLGym/Examples/Traffic/Experiments/E03Calibration/impsmp.py
+++ pyRDDLGym/Examples/Traffic/Experiments/E03Calibration/impsmp.py
@@ -27,6 +27,10 @@ jnp.set_printoptions(
     linewidth=9999,
     formatter={'float': lambda x: "{0:0.3f}".format(x)})
 
+profile = True
+if profile:
+    import jax.profiler as jprof
+
 key = jax.random.PRNGKey(3264)
 
 
@@ -56,10 +60,10 @@ full_cycle = all_red_stretch + [1.0] + [0.0]*59 + all_red_stretch + [1.0] + [0.0
 BASE_PHASING = jnp.array(full_cycle*10, dtype=jnp.float64)
 FIXED_TIME_PLAN = jnp.broadcast_to(BASE_PHASING[..., None], shape=(BASE_PHASING.shape[0], N))
 
-
 def policy_fn(key, policy_params, hyperparams, step, states):
     """ Each traffic light is assumed to follow a fixed-time plan """
     return {'advance': FIXED_TIME_PLAN[step]}
+
 
 # compile batch and sequential rollout samplers
 n_rollouts = 128
@@ -89,7 +93,7 @@ for (state, next_state) in model.next_state.items():
     subs_hmc[next_state] = subs_hmc[state]
 
 
-# ground truth rates
+# set up ground truth inflow rates
 source_indices = range(16,24)
 true_source_rates = jnp.array([0.3, 0.2, 0.1, 0.4, 0.1, 0.2, 0.3])
 num_nonzero_sources = 2
@@ -101,10 +105,17 @@ one_hot_inputs = jnp.eye(num_nonzero_sources)
 
 with jax.disable_jit(disable=False):
     t0 = timer()
+    if profile:
+        jprof.start_trace('/tmp/tensorboard')
 
     # Prepare the ground truth inflow rates
     nulled_rates = jnp.zeros(shape=(n_rollouts, s2-s1))
     base_rates = jnp.broadcast_to(source_rates[None,...], shape=(n_rollouts,s1-s0))
+
+    # The difference in the batched and sequential rollout samplers
+    # is in the shapes of the argument and return arrays. Because
+    # jit-compilation requires static array shapes, the two sampler
+    # types are compiled separately
 
     # Prepare the ground truth for batched rollouts
     subs_train['SOURCE-ARRIVAL-RATE'] = subs_train['SOURCE-ARRIVAL-RATE'].at[:,s0:s1].set(base_rates)
@@ -135,8 +146,7 @@ with jax.disable_jit(disable=False):
         # Currently assumes a multivariate normal policy with
         # a diagonal covariance matrix
 
-#        last_layer_bias_init = hk.initializers.Constant(jnp.array([jnp.log(3), 0.]))
-        last_layer_bias_init = hk.initializers.Constant(jnp.array([-1.821, -1.82118]))
+        last_layer_bias_init = hk.initializers.Constant(jnp.array([-1.821, -1.82118], dtype=compiler.REAL))
         mlp = hk.Sequential([
             hk.Linear(32), jax.nn.relu,
             hk.Linear(32), jax.nn.relu,
@@ -212,7 +222,10 @@ with jax.disable_jit(disable=False):
         return C
 
 
-    def reinforce(key, n_iters, theta, subs_train, input, subs_hmc=None, est_Z=None):
+    def reinforce(key, n_iters, theta, subs_train, subs_hmc=None, est_Z=None):
+        # (The arguments that default to 'None' are included to have a consistent signature
+        #  with the impsmp function)
+
         # initialize stats collection
         X, Y = np.arange(n_iters), np.zeros(shape=(n_iters,3,num_nonzero_sources))
         n_params = sum(leaf.flatten().shape[0] for leaf in jax.tree_util.tree_leaves(theta))
@@ -224,12 +237,16 @@ with jax.disable_jit(disable=False):
         opt_state = optimizer.init(theta)
 
         # run
+        key, subkey = jax.random.split(key)
+        mean, cov = policy_apply(theta, subkey, one_hot_inputs)
+
         for idx in range(n_iters):
+            subt0 = timer()
+
             grads = jax.tree_util.tree_map(lambda _: jnp.zeros(1), theta)
             cmlt_rewards = 0
             for _ in range(n_batches):
                 key, *subkeys = jax.random.split(key, num=3)
-                mean, cov = policy_apply(theta, subkeys[0], one_hot_inputs)
                 actions = policy_sample(theta, subkeys[0])
                 pi = policy_pdf(theta, subkeys[0], actions)
                 dpi = jax.jacrev(policy_pdf, argnums=0)(theta, subkeys[0], actions)
@@ -248,17 +265,22 @@ with jax.disable_jit(disable=False):
             Y[idx,0] = cmlt_rewards
             Y[idx,1] = mean
             Y[idx,2] = jnp.diag(cov)
-            print(idx, cmlt_rewards, Y[idx])
+            subt1 = timer()
+            print(f'Iter {idx} :: REINFORCE :: Runtime={subt1-subt0}s')
+            print(f'[Mean, Diag(Cov)] = \n{Y[idx,1:]}')
+            print(f'Eval. reward={Y[idx,0,0]}\n')
 
             C = collect_covar_data(theta, C, idx)
 
         return X, Y, C
 
 
-    # === REINFORCE + Importance sampling ====
+    # === REINFORCE with Importance Sampling ====
     impsmp_num_iters = int(n_rollouts * n_batches)
     impsmp_num_burnin_steps = int(impsmp_num_iters/8)
     impsmp_step_size = 0.1
+    impsmp_lr = 1e-3
+    impsmp_momentum = 0.1
 
 
     @jax.jit
@@ -275,7 +297,7 @@ with jax.disable_jit(disable=False):
         unnormalized_log_rho, (None, None, None, 0), 0))
 
 
-    def impsmp(key, n_iters, theta, subs_train, input, subs_hmc, est_Z=False):
+    def impsmp(key, n_iters, theta, subs_train, subs_hmc, est_Z=False):
 
         # initialize stats collection
         X, Y = np.arange(n_iters), np.zeros(shape=(n_iters, 3, num_nonzero_sources))
@@ -291,11 +313,14 @@ with jax.disable_jit(disable=False):
 
         # initialize optimizer
         #optimizer = optax.sgd(learning_rate=1e-3)
-        optimizer = optax.rmsprop(learning_rate=1e-3)
+        optimizer = optax.rmsprop(
+            learning_rate=impsmp_lr,
+            momentum=impsmp_momentum)
         opt_state = optimizer.init(theta)
 
         # initialize unconstraining bijector
         unconstraining_bijector = [
+            #tfp.bijectors.SoftClip(low=0., high=0.4)
             #tfp.bijectors.Softplus()
             tfp.bijectors.Identity()
         ]
@@ -308,14 +333,14 @@ with jax.disable_jit(disable=False):
             log_density = functools.partial(
                 unnormalized_log_rho, subkeys[0], theta, subs_hmc)
 
-            adaptive_hmc_kernel = tfp.mcmc.SimpleStepSizeAdaptation(
-                tfp.mcmc.TransformedTransitionKernel(
+            adaptive_hmc_kernel = tfp.mcmc.TransformedTransitionKernel(
+                inner_kernel = tfp.mcmc.SimpleStepSizeAdaptation(
                     inner_kernel=tfp.mcmc.HamiltonianMonteCarlo(
                         target_log_prob_fn=log_density,
                         num_leapfrog_steps=3,
                         step_size=impsmp_step_size),
-                    bijector=unconstraining_bijector),
-                num_adaptation_steps=int(impsmp_num_burnin_steps * 0.8))
+                    num_adaptation_steps=int(impsmp_num_burnin_steps * 0.8)),
+                bijector=unconstraining_bijector)
 
             samples, is_accepted = tfp.mcmc.sample_chain(
                 seed=subkeys[1],
@@ -327,7 +352,6 @@ with jax.disable_jit(disable=False):
 
             grads = jax.tree_util.tree_map(lambda _: jnp.zeros(1), theta)
             cmlt_rewards = 0.
-
             for si in range(n_batches):
                 actions = samples[si*n_rollouts:(si+1)*n_rollouts]
                 dpi = jax.jacrev(policy_pdf, argnums=0)(theta, subkeys[2], actions)
@@ -355,30 +379,91 @@ with jax.disable_jit(disable=False):
 
             mean, cov = policy_apply(theta, subkeys[0], one_hot_inputs)
             eval_actions = policy_sample(theta, subkeys[0])
-            eval_rewards = reward_batched(subkeys[0], subs_train, eval_actions)
+            eval_rewards = jnp.mean(reward_batched(subkeys[0], subs_train, eval_actions))
 
-            Y[idx,0] = cmlt_rewards
+            Y[idx,0,0] = eval_rewards
+            Y[idx,0,1] = cmlt_rewards
             Y[idx,1] = mean
             Y[idx,2] = jnp.diag(cov)
             subt1 = timer()
-            print(f'Iter {idx} :: Runtime={subt1-subt0}s ::  HMC acceptance rate={jnp.mean(is_accepted)*100:.2f}%')
-            print(f'[Rew, Mean, Cov] = \n{Y[idx]}')
-            print(f'Eval rew.: {jnp.mean(eval_rewards)}\n')
+            print(f'Iter {idx} :: Importance Sampling :: Runtime={subt1-subt0}s :: HMC acceptance rate={jnp.mean(is_accepted)*100:.2f}%')
+            print(f'[Mean, Diag(Cov)] = \n{Y[idx,1:]}')
+            print(f'HMC sample reward={Y[idx,0,1]} :: Eval reward={Y[idx,0,0]}\n')
 
             C = collect_covar_data(theta, C, idx)
 
         return X, Y, C
 
+    # === REINFORCE with Random Sampling (Sanity check) ===
+    def rndsmp(key, n_iters, theta, subs_train, subs_hmc=None, est_Z=None):
+        # Sanity check benchmark. How well does randomly sampling the actions perform?
+        # We often observe that the means learn to become small and variances large.
+
+        # initialize stats collection
+        X, Y = np.arange(n_iters), np.zeros(shape=(n_iters,3,num_nonzero_sources))
+        n_params = sum(leaf.flatten().shape[0] for leaf in jax.tree_util.tree_leaves(theta))
+        C = jnp.zeros(shape=(n_params, n_iters))
+
+        # initialize optimizer
+        #optimizer = optax.sgd(learning_rate=1e-5)
+        optimizer = optax.rmsprop(learning_rate=1e-3)
+        opt_state = optimizer.init(theta)
+
+        # run
+        for idx in range(n_iters):
+            subt0 = timer()
+            grads = jax.tree_util.tree_map(lambda _: jnp.zeros(1), theta)
+            cmlt_rewards = 0
+            for _ in range(n_batches):
+                key, *subkeys = jax.random.split(key, num=3)
+                actions = jax.random.uniform(
+                    subkeys[0],
+                    shape=(n_rollouts, num_nonzero_sources),
+                    minval=0., maxval=0.4)
+                pi = policy_pdf(theta, subkeys[1], actions)
+                dpi = jax.jacrev(policy_pdf, argnums=0)(theta, subkeys[1], actions)
+                rewards = reward_batched(subkeys[1], subs_train, actions)
+                factors = rewards / (pi + 1e-6)
+
+                w = lambda x: jnp.mean(weighting_map(factors, x), axis=0)
+
+                accumulant = jax.tree_util.tree_map(w, dpi)
+                grads = jax.tree_util.tree_map(lambda x, y: x+(y/n_batches), grads, accumulant)
+                cmlt_rewards += jnp.mean(rewards)/n_batches
+
+            updates, opt_state = optimizer.update(grads, opt_state)
+            theta = optax.apply_updates(theta, updates)
+
+            key, subkey = jax.random.split(key)
+            mean, cov = policy_apply(theta, subkey, one_hot_inputs)
+            eval_actions = policy_sample(theta, subkey)
+            eval_rewards = jnp.mean(reward_batched(subkey, subs_train, eval_actions))
+
+            Y[idx,0,0] = eval_rewards
+            Y[idx,0,1] = cmlt_rewards
+            Y[idx,1] = mean
+            Y[idx,2] = jnp.diag(cov)
+            subt1 = timer()
+            print(f'Iter {idx} :: Random Sampling :: Runtime={subt1-subt0}s')
+            print(f'[Mean, Diag(Cov)] = \n{Y[idx,1:]}')
+            print(f'Random sample reward={Y[idx,0,1]} :: Eval. reward={Y[idx,0,0]}\n')
+
+            C = collect_covar_data(theta, C, idx)
+
+        return X, Y, C
+
+
     def eval_single_rollout(a):
         # For debugging
         a = jnp.asarray(a)
         rewards = reward_seqtl(key, subs_hmc, a)
-        print(rewards)
+        return rewards
 
 
-    method = 'reinforce'
+    method = 'impsmp'
     if method == 'impsmp': method_fn = impsmp
     elif method == 'reinforce': method_fn = reinforce
+    elif method == 'rndsmp': method_fn = rndsmp
     else: raise KeyError
 
 
@@ -388,23 +473,32 @@ with jax.disable_jit(disable=False):
 
     key, subkey = jax.random.split(key)
     t1 = timer()
-    X, Y, C = method_fn(key=subkey, n_iters=500, theta=theta, subs_train=subs_train, input=input, subs_hmc=subs_hmc, est_Z=True)
+    X, Y, C = method_fn(key=subkey, n_iters=250, theta=theta, subs_train=subs_train, subs_hmc=subs_hmc, est_Z=True)
     t2 = timer()
 
-    Covar = jnp.cov(C)
-    sns.heatmap(Covar)
+    X.block_until_ready()
+
+    thetacov = jnp.cov(C)
+    sns.heatmap(thetacov)
     plt.savefig(f'img/{timestamp}_{id}_covar.png')
 
     with open(f'img/{timestamp}_{id}.json', 'w') as file:
-        json.dump({
+        saveddata = {
             'num_nonzero_sources': num_nonzero_sources,
-            'impsmp_num_iters': impsmp_num_iters,
-            'impsmp_num_burnin_steps': impsmp_num_burnin_steps,
-            'impsmp_step_size': impsmp_step_size,
             'X': X.tolist(),
             'Y': Y.T.tolist(),
             'C': C.tolist(),
-            'Covar': Covar.tolist()}, file)
+            'thetacov': thetacov.tolist()}
+        if method == 'impsmp':
+            saveddata.update({
+                'impsmp_num_iters': impsmp_num_iters,
+                'impsmp_num_burnin_steps': impsmp_num_burnin_steps,
+                'impsmp_step_size': impsmp_step_size,
+                'impsmp_lr': impsmp_lr,
+                'impsmp_momentum': impsmp_momentum,})
+        json.dump(datadict, file)
 
+    if profile:
+        jprof.stop_trace()
     print('Setup took', t1-t0, 'seconds')
     print('Optimization took', t2-t1, 'seconds')
