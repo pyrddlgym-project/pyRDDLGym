48e0ee570dbd899b18cd99c03c39378fd9f4e037
Ilia Smirnov <iliathesmirnov@gmail.com>
2023-08-02 19:49:41 -0400

    (Importance Sampling) Converted impsmp.py to work with the OD setting. Old flow setting migrated to flow_impsmp.py

--- pyRDDLGym/Examples/Traffic/Experiments/E03Calibration/impsmp.py
+++ pyRDDLGym/Examples/Traffic/Experiments/E03Calibration/impsmp.py
@@ -18,6 +18,9 @@ from pyRDDLGym import RDDLEnv
 from pyRDDLGym.Core.Jax.JaxRDDLLogic import FuzzyLogic
 from pyRDDLGym.Core.Jax.JaxRDDLBackpropPlanner import JaxRDDLCompilerWithGrad
 
+from od_to_flows_and_turns import prepare_shortest_path_assignment
+from od_to_flows_and_turns import convert_od_to_flows_and_turn_props_jax
+
 
 from jax.config import config as jconfig
 jconfig.update('jax_debug_nans', True)
@@ -62,7 +65,7 @@ def policy_fn(key, policy_params, hyperparams, step, states):
     return {'advance': FIXED_TIME_PLAN[step]}
 
 
-# compile batch and sequential rollout samplers
+# jit-compile batch and sequential rollout samplers
 n_rollouts = 128
 n_batches = 1
 sampler = compiler.compile_rollouts(policy=policy_fn,
@@ -91,21 +94,22 @@ for (state, next_state) in model.next_state.items():
 
 
 # set up ground truth inflow rates
-source_indices = range(16,24)
-true_source_rates = jnp.array([0.3, 0.2, 0.1, 0.4, 0.1, 0.2, 0.3])
-num_nonzero_sources = 2
-source_rates = true_source_rates[:num_nonzero_sources]
-s0, s1, s2 = source_indices[0], source_indices[num_nonzero_sources], source_indices[-1]
+srcs, sinks, turn_flow_matrix = prepare_shortest_path_assignment(myEnv)
+turn_flow_matrix = jnp.array(turn_flow_matrix)[jnp.newaxis, ...]
 
-one_hot_inputs = jnp.eye(num_nonzero_sources)
+s0, s1 = 16, 24 # The range of source link indices among all link indices
+ground_truth_od = np.zeros(shape=(len(srcs),len(sinks)))
+ground_truth_od[0,4] = 0.3
+ground_truth_od = jnp.array(ground_truth_od)[jnp.newaxis, ...]
 
+action_dim = len(srcs)*len(sinks)
+one_hot_inputs = jnp.eye(action_dim)
 
 with jax.disable_jit(disable=False):
     t0 = timer()
 
-    # Prepare the ground truth inflow rates
-#    nulled_rates = jnp.zeros(shape=(n_rollouts, s2-s1))
-    base_rates = jnp.broadcast_to(source_rates[None,...], shape=(n_rollouts,s1-s0))
+    ground_truth_inflows, ground_truth_turn_props = convert_od_to_flows_and_turn_props_jax(
+        ground_truth_od, turn_flow_matrix)
 
     # The difference in the batched and sequential rollout samplers
     # is in the shapes of the argument and return arrays. Because
@@ -113,8 +117,15 @@ with jax.disable_jit(disable=False):
     # types are compiled separately
 
     # Prepare the ground truth for batched rollouts
-    subs_train['SOURCE-ARRIVAL-RATE'] = subs_train['SOURCE-ARRIVAL-RATE'].at[:,s0:s1].set(base_rates)
-    subs_train['SOURCE-ARRIVAL-RATE'] = subs_train['SOURCE-ARRIVAL-RATE'].at[:,s1:s2].set(0.)
+    batch_ground_inflows = jnp.broadcast_to(
+        ground_truth_inflows,
+        shape=(n_rollouts, s1-s0))
+    batch_ground_turn_props = jnp.broadcast_to(
+        ground_truth_turn_props,
+        shape=(n_rollouts, 24, 24))
+
+    subs_train['SOURCE-ARRIVAL-RATE'] = subs_train['SOURCE-ARRIVAL-RATE'].at[:,s0:s1].set(batch_ground_inflows)
+    subs_train['BETA'] = batch_ground_turn_props
     rollouts = sampler(
             key,
             policy_params=None,
@@ -124,8 +135,9 @@ with jax.disable_jit(disable=False):
     GROUND_TRUTH_LOOPS = rollouts['pvar']['flow-into-link'][:,:,:]
 
     # Prepare the ground truth for sequential rollouts
-    subs_hmc['SOURCE-ARRIVAL-RATE'] = subs_hmc['SOURCE-ARRIVAL-RATE'].at[:,s0:s1].set(source_rates)
-    subs_hmc['SOURCE-ARRIVAL-RATE'] = subs_hmc['SOURCE-ARRIVAL-RATE'].at[:,s1:s2].set(0.)
+    subs_hmc['SOURCE-ARRIVAL-RATE'] = subs_hmc['SOURCE-ARRIVAL-RATE'].at[:,s0:s1].set(ground_truth_inflows)
+    subs_hmc['BETA'] = ground_truth_turn_props
+
     rollout_hmc = sampler_hmc(
             key,
             policy_params=None,
@@ -133,14 +145,11 @@ with jax.disable_jit(disable=False):
             subs=subs_hmc,
             model_params=compiler.model_params)
     GROUND_TRUTH_LOOPS_HMC = rollout_hmc['pvar']['flow-into-link'][:,:,:]
-    vrates = jnp.copy(subs_train['SOURCE-ARRIVAL-RATE'])
-
 
 
     def parametrized_policy(input):
         # Currently assumes a multivariate normal policy with
         # a diagonal covariance matrix
-
         last_layer_bias_init = hk.initializers.Constant(jnp.array([-1.821, -1.82118], dtype=compiler.REAL))
         mlp = hk.Sequential([
             hk.Linear(32), jax.nn.relu,
@@ -175,7 +184,10 @@ with jax.disable_jit(disable=False):
 
     @jax.jit
     def reward_batched(rng, subs, actions):
-        subs['SOURCE-ARRIVAL-RATE'] = subs['SOURCE-ARRIVAL-RATE'].at[:,s0:s1].set(actions)
+        od = actions.reshape(n_rollouts, len(srcs), len(sinks))
+        inflows, turn_props = convert_od_to_flows_and_turn_props_jax(od, turn_flow_matrix)
+        subs['SOURCE-ARRIVAL-RATE'] = subs['SOURCE-ARRIVAL-RATE'].at[:,s0:s1].set(inflows)
+        subs['BETA'] = turn_props
         rollouts = sampler(
             rng,
             policy_params=None,
@@ -187,7 +199,10 @@ with jax.disable_jit(disable=False):
 
     @jax.jit
     def reward_seqtl(rng, subs, action):
-        subs['SOURCE-ARRIVAL-RATE'] = subs['SOURCE-ARRIVAL-RATE'].at[:,s0:s1].set(action)
+        od = action.reshape(1, len(srcs), len(sinks))
+        inflows, turn_props = convert_od_to_flows_and_turn_props_jax(od, turn_flow_matrix)
+        subs['SOURCE-ARRIVAL-RATE'] = subs['SOURCE-ARRIVAL-RATE'].at[:,s0:s1].set(inflows)
+        subs['BETA'] = turn_props
         rollout = sampler_hmc(
             rng,
             policy_params=None,
@@ -221,25 +236,23 @@ with jax.disable_jit(disable=False):
         # (The arguments that default to 'None' are included to have a consistent signature
         #  with the impsmp function)
 
-        # initialize stats collection
-        X, Y = np.arange(n_iters), np.zeros(shape=(n_iters,3,num_nonzero_sources))
-        n_params = sum(leaf.flatten().shape[0] for leaf in jax.tree_util.tree_leaves(theta))
-        C = jnp.zeros(shape=(n_params, n_iters))
-
         # initialize optimizer
         #optimizer = optax.sgd(learning_rate=1e-5)
-        optimizer = optax.rmsprop(learning_rate=1e-3)
+        optimizer = optax.rmsprop(learning_rate=1e-3, momentum=0.1)
         opt_state = optimizer.init(theta)
 
-        # run
-        key, subkey = jax.random.split(key)
-        mean, cov = policy_apply(theta, subkey, one_hot_inputs)
+        # initialize stats
+        stats = np.zeros(shape=(n_iters,))
 
+        # run
         for idx in range(n_iters):
             subt0 = timer()
 
+            key, subkey = jax.random.split(key)
+            mean, cov = policy_apply(theta, subkey, one_hot_inputs)
+
             grads = jax.tree_util.tree_map(lambda _: jnp.zeros(1), theta)
-            cmlt_rewards = 0
+            cmlt_rewards = 0.
             for _ in range(n_batches):
                 key, *subkeys = jax.random.split(key, num=3)
                 actions = policy_sample(theta, subkeys[0])
@@ -257,35 +270,32 @@ with jax.disable_jit(disable=False):
             updates, opt_state = optimizer.update(grads, opt_state)
             theta = optax.apply_updates(theta, updates)
 
-            Y[idx,0] = cmlt_rewards
-            Y[idx,1] = mean
-            Y[idx,2] = jnp.diag(cov)
+            stats[idx] = cmlt_rewards
             subt1 = timer()
             print(f'Iter {idx} :: REINFORCE :: Runtime={subt1-subt0}s')
-            print(f'[Mean, Diag(Cov)] = \n{Y[idx,1:]}')
-            print(f'Eval. reward={Y[idx,0,0]}\n')
+            print(f'Eval. reward={stats[idx]}\n')
 
-            C = collect_covar_data(theta, C, idx)
-
-        return X, Y, C
+        return stats
 
 
     # === REINFORCE with Importance Sampling ====
-    impsmp_params = {
+    impsmp_config = {
         'num_iters': int(n_rollouts * n_batches),
-        'step_size': 0.1,
+        'step_size': 0.25,
+        'optimizer': 'rmsprop',
         'lr': 1e-3,
         'momentum': 0.1,
+        'est_Z': False,
     }
-    impsmp_params['num_burnin_steps'] = int(impsmp_params['num_iters']/8)
+    impsmp_config['num_burnin_steps'] = int(impsmp_config['num_iters']/8)
 
 
     @jax.jit
     def unnormalized_log_rho(key, theta, subs, a):
         #dpi = jax.jacrev(policy_pdf, argnums=0)(theta, key, a)
         dpi = jax.grad(policy_pdf, argnums=0)(theta, key, a)
-        #dpi_norm = jax.tree_util.tree_reduce(lambda x,y: x + jnp.sum(y*y), dpi, initializer=jnp.zeros(1))
-        #dpi_norm = jnp.sqrt(dpi_norm)
+        #dpi_norm = jax.tree_util.tree_reduce(lambda x,y: x + jnp.sum((y/100)**2), dpi, initializer=jnp.zeros(1))
+        #dpi_norm = 100*jnp.sqrt(dpi_norm)
         dpi_norm = jax.tree_util.tree_reduce(lambda x, y: jnp.maximum(x, jnp.max(jnp.abs(y))), dpi, initializer=jnp.array([-jnp.inf]))
         rewards = reward_seqtl(key, subs, a)
         density = jnp.abs(rewards) * dpi_norm
@@ -297,23 +307,26 @@ with jax.disable_jit(disable=False):
 
     def impsmp(key, n_iters, theta, subs_train, subs_hmc, est_Z=False):
 
-        # initialize stats collection
-        X, Y = np.arange(n_iters), np.zeros(shape=(n_iters, 3, num_nonzero_sources))
-        n_params = sum(leaf.flatten().shape[0] for leaf in jax.tree_util.tree_leaves(theta))
-        C = jnp.zeros(shape=(n_params, n_iters))
+        # initialize stats
+        stats = np.zeros(shape=(n_iters,2))
 
-        # hmc initial points
+        # initialize hmc
         key, subkey = jax.random.split(key, num=2)
         hmc_initializer = jax.random.uniform(
             subkey,
-            shape=(num_nonzero_sources,),
-            minval=0.05, maxval=0.35)
+            shape=(action_dim,),
+            minval=0.05, maxval=0.15)
 
         # initialize optimizer
-        #optimizer = optax.sgd(learning_rate=1e-3)
-        optimizer = optax.rmsprop(
-            learning_rate=impsmp_params['lr'],
-            momentum=impsmp_params['momentum'])
+        if impsmp_config['optimizer'] == 'rmsprop':
+            optimizer = optax.rmsprop(
+                learning_rate=impsmp_config['lr'], momentum=impsmp_config['momentum'])
+        elif impsmp_config['optimizer'] == 'adam':
+            optimizer = optax.adam(learning_rate=impsmp_config['lr'])
+        elif impsmp_config['optimizer'] == 'sgd':
+            optimizer = optax.sgd(
+                learning_rate=impsmp_config['lr'], momentum=impsmp_config['momentum'])
+        else: raise ValueError
         opt_state = optimizer.init(theta)
 
         # initialize unconstraining bijector
@@ -337,14 +350,14 @@ with jax.disable_jit(disable=False):
                     inner_kernel=tfp.mcmc.HamiltonianMonteCarlo(
                         target_log_prob_fn=log_density,
                         num_leapfrog_steps=3,
-                        step_size=impsmp_params['step_size']),
-                    num_adaptation_steps=int(impsmp_params['num_burnin_steps'] * 0.8)),
+                        step_size=impsmp_config['step_size']),
+                    num_adaptation_steps=int(impsmp_config['num_burnin_steps'] * 0.8)),
                 bijector=unconstraining_bijector)
 
             samples, is_accepted = tfp.mcmc.sample_chain(
                 seed=subkeys[1],
-                num_results=impsmp_params['num_iters'],
-                num_burnin_steps=impsmp_params['num_burnin_steps'],
+                num_results=impsmp_config['num_iters'],
+                num_burnin_steps=impsmp_config['num_burnin_steps'],
                 current_state=hmc_initializer,
                 kernel=adaptive_hmc_kernel,
                 trace_fn=lambda _, pkr: pkr.inner_results.inner_results.is_accepted)
@@ -380,77 +393,13 @@ with jax.disable_jit(disable=False):
             eval_actions = policy_sample(theta, subkeys[0])
             eval_rewards = jnp.mean(reward_batched(subkeys[0], subs_train, eval_actions))
 
-            Y[idx,0,0] = eval_rewards
-            Y[idx,0,1] = cmlt_rewards
-            Y[idx,1] = mean
-            Y[idx,2] = jnp.diag(cov)
+            stats[idx,0] = eval_rewards
+            stats[idx,1] = cmlt_rewards
             subt1 = timer()
             print(f'Iter {idx} :: Importance Sampling :: Runtime={subt1-subt0}s :: HMC acceptance rate={jnp.mean(is_accepted)*100:.2f}%')
-            print(f'[Mean, Diag(Cov)] = \n{Y[idx,1:]}')
-            print(f'HMC sample reward={Y[idx,0,1]} :: Eval reward={Y[idx,0,0]}\n')
+            print(f'HMC sample reward={stats[idx,1]} :: Eval reward={stats[idx,0]}\n')
 
-            C = collect_covar_data(theta, C, idx)
-
-        return X, Y, C
-
-    # === REINFORCE with Random Sampling (Sanity check) ===
-    def rndsmp(key, n_iters, theta, subs_train, subs_hmc=None, est_Z=None):
-        # Sanity check benchmark. How well does randomly sampling the actions perform?
-        # We often observe that the means learn to become small and variances large.
-
-        # initialize stats collection
-        X, Y = np.arange(n_iters), np.zeros(shape=(n_iters,3,num_nonzero_sources))
-        n_params = sum(leaf.flatten().shape[0] for leaf in jax.tree_util.tree_leaves(theta))
-        C = jnp.zeros(shape=(n_params, n_iters))
-
-        # initialize optimizer
-        #optimizer = optax.sgd(learning_rate=1e-5)
-        optimizer = optax.rmsprop(learning_rate=1e-3)
-        opt_state = optimizer.init(theta)
-
-        # run
-        for idx in range(n_iters):
-            subt0 = timer()
-            grads = jax.tree_util.tree_map(lambda _: jnp.zeros(1), theta)
-            cmlt_rewards = 0
-            for _ in range(n_batches):
-                key, *subkeys = jax.random.split(key, num=3)
-                actions = jax.random.uniform(
-                    subkeys[0],
-                    shape=(n_rollouts, num_nonzero_sources),
-                    minval=0., maxval=0.4)
-                pi = policy_pdf(theta, subkeys[1], actions)
-                dpi = jax.jacrev(policy_pdf, argnums=0)(theta, subkeys[1], actions)
-                rewards = reward_batched(subkeys[1], subs_train, actions)
-                factors = rewards / (pi + 1e-6)
-
-                w = lambda x: jnp.mean(weighting_map(factors, x), axis=0)
-
-                accumulant = jax.tree_util.tree_map(w, dpi)
-                grads = jax.tree_util.tree_map(lambda x, y: x+(y/n_batches), grads, accumulant)
-                cmlt_rewards += jnp.mean(rewards)/n_batches
-
-            updates, opt_state = optimizer.update(grads, opt_state)
-            theta = optax.apply_updates(theta, updates)
-
-            key, subkey = jax.random.split(key)
-            mean, cov = policy_apply(theta, subkey, one_hot_inputs)
-            eval_actions = policy_sample(theta, subkey)
-            eval_rewards = jnp.mean(reward_batched(subkey, subs_train, eval_actions))
-
-            Y[idx,0,0] = eval_rewards
-            Y[idx,0,1] = cmlt_rewards
-            Y[idx,1] = mean
-            Y[idx,2] = jnp.diag(cov)
-            subt1 = timer()
-            print(f'Iter {idx} :: Random Sampling :: Runtime={subt1-subt0}s')
-            print(f'[Mean, Diag(Cov)] = \n{Y[idx,1:]}')
-            print(f'Random sample reward={Y[idx,0,1]} :: Eval. reward={Y[idx,0,0]}\n')
-
-            C = collect_covar_data(theta, C, idx)
-
-        return X, Y, C
-
+        return stats
 
     def eval_single_rollout(a):
         # For debugging
@@ -466,16 +415,19 @@ with jax.disable_jit(disable=False):
     else: raise KeyError
 
 
-    id = f'{method}_2x2_b{n_rollouts*n_batches}_nS{num_nonzero_sources}'
+    id = f'{method}_2x2_b{n_rollouts*n_batches}_od'
     timestamp = datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
 
     key, subkey = jax.random.split(key)
     t1 = timer()
-    X, Y, C = method_fn(key=subkey, n_iters=10, theta=theta, subs_train=subs_train, subs_hmc=subs_hmc, est_Z=True)
+    stats = method_fn(key=subkey, n_iters=100, theta=theta, subs_train=subs_train, subs_hmc=subs_hmc, est_Z=impsmp_config['est_Z'])
     t2 = timer()
 
-    C.block_until_ready()
+    fig, ax = plt.subplots()
+    ax.plot(range(stats.shape[0]), stats)
+    plt.savefig('img/{timestamp}_lc.png')
 
+    """
     thetacov = jnp.cov(C)
     sns.heatmap(thetacov)
     plt.savefig(f'img/{timestamp}_{id}_covar.png')
@@ -484,12 +436,13 @@ with jax.disable_jit(disable=False):
         savedata = {
             'num_nonzero_sources': num_nonzero_sources,
             'X': X.tolist(),
-            'Y': Y.T.tolist(),
-            'C': C.tolist(),
-            'thetacov': thetacov.tolist()}
+            'Y': Y.T.tolist(),}
+            #'C': C.tolist(),
+            #'thetacov': thetacov.tolist()}
         if method == 'impsmp':
-            savedata.update(impsmp_params)
+            savedata.update(impsmp_config)
         json.dump(savedata, file)
 
     print('Setup took', t1-t0, 'seconds')
     print('Optimization took', t2-t1, 'seconds')
+    """
