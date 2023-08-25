f9a9a61c8edee4ac906616cb755d6c3c8ddc502d
Mike Gimelfarb <35513382+mike-gimelfarb@users.noreply.github.com>
2023-08-25 07:15:45 -0400

    Clean up gym example

--- pyRDDLGym/GymExample.py
+++ pyRDDLGym/GymExample.py
@@ -1,47 +1,40 @@
 import sys
-import os
 
-from pyRDDLGym import RDDLEnv
 from pyRDDLGym import ExampleManager
+from pyRDDLGym import RDDLEnv
 from pyRDDLGym.Policies.Agents import RandomAgent
-from pyRDDLGym.Visualizer.MovieGenerator import MovieGenerator
-from pyRDDLGym.Visualizer.TextViz import TextVisualizer
 
 def main(env, inst, method_name=None, episodes=1):
     print(f'preparing to launch instance {inst} of domain {env}...')
     
     # get the environment info
-    # ExampleManager.RebuildExamples()
     EnvInfo = ExampleManager.GetEnvInfo(env)
     
-    # set up the environment class, choose instance 0 because every example has at least one example instance
+    # set up the environment class
     log = False if method_name is None else True
     myEnv = RDDLEnv.RDDLEnv(domain=EnvInfo.get_domain(), 
                             instance=EnvInfo.get_instance(inst),
                             enforce_action_constraints=False,
-                            debug=False,
                             log=log,
                             simlogname=method_name)
     myEnv.seed(42)
     
     # set up the environment visualizer
-    # frames_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'Visualizer', 'Frames')
-    myEnv.set_visualizer(EnvInfo.get_visualizer())#, movie_gen=MovieGenerator(frames_path, env, 200), movie_per_episode=True)
-    # myEnv.set_visualizer(None, movie_gen=MovieGenerator(frames_path, env, 200),
-    #                      movie_per_episode=True)
+    myEnv.set_visualizer(EnvInfo.get_visualizer())
     
-    # set up an example aget
+    # set up an example agent
     agent = RandomAgent(action_space=myEnv.action_space, 
                         num_actions=myEnv.numConcurrentActions,
                         seed=42)
-
+    
+    # main simulation loop
     for episode in range(episodes):
         total_reward = 0
         state = myEnv.reset()
         for step in range(myEnv.horizon):
             myEnv.render()
             action = agent.sample_action()
-            next_state, reward, done, info = myEnv.step(action)
+            next_state, reward, done, _ = myEnv.step(action)
             total_reward += reward
             print()
             print(f'step       = {step}')
