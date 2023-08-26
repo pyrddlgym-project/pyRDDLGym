9222740498762e34969c72236aff85c3a7f923fd
Mike Gimelfarb <35513382+mike-gimelfarb@users.noreply.github.com>
2023-08-26 17:16:23 -0400

    Basic quadcopter domain/instance, viz and jax config

---
+++ pyRDDLGym/Visualizer/QuadcopterViz.py
@@ -0,0 +1,81 @@
+import io
+import matplotlib.pyplot as plt
+import mpl_toolkits.mplot3d.axes3d as p3
+import numpy as np
+from PIL import Image
+import pygame
+
+from pyRDDLGym.Core.Compiler.RDDLModel import PlanningModel
+from pyRDDLGym.Visualizer.StateViz import StateViz
+
+
+class QuadcopterVisualizer(StateViz):
+
+    def __init__(self, model: PlanningModel,
+                 bound=5,
+                 figure_size=(6, 6),
+                 wait_time=100) -> None:
+        self._model = model
+        self._bound = bound
+        self._figure_size = figure_size
+        self._wait_time = wait_time
+        
+        self._objects = model.objects
+        self._nonfluents = model.groundnonfluents()
+        
+        self.fig = plt.figure(figsize=self._figure_size)
+        self.ax = self.fig.add_subplot(111, projection='3d')
+        
+    def convert2img(self):
+        img_buf = io.BytesIO()
+        self.fig.savefig(img_buf, format='png')
+        im = Image.open(img_buf)
+        return im
+
+    def render(self, state):
+        scale = 1
+        
+        self.ax.set_xlim3d([-self._bound, self._bound])
+        self.ax.set_ylim3d([-self._bound, self._bound])
+        self.ax.set_zlim3d([-self._bound, self._bound])
+        
+        for dk in self._objects['drone']:
+            pos = np.array([state[f'x___{dk}'], state[f'y___{dk}'], state[f'z___{dk}']])
+            phi, theta, psi = state[f'phi___{dk}'], state[f'theta___{dk}'], state[f'psi___{dk}']
+            cph, cth, cps = np.cos(phi), np.cos(theta), np.cos(psi)
+            sph, sth, sps = np.sin(phi), np.sin(theta), np.sin(psi)        
+            R = np.array([
+                [cph * cps - cth * sth * sps, -cps * sph - cph * cth * sps, sth * sps],
+                [cth * cps * sph + cph * sps, cph * cth * cps - sph * sps, -cps * sth],
+                [sph * sth, cph * sth, cth]
+            ])
+            l = scale * self._nonfluents['L']
+            motor1 = (R @ np.array([[l], [0], [0]])).reshape((-1,)) + pos
+            motor2 = (R @ np.array([[-l], [0], [0]])).reshape((-1,)) + pos
+            motor3 = (R @ np.array([[0], [l], [0]])).reshape((-1,)) + pos
+            motor4 = (R @ np.array([[0], [-l], [0]])).reshape((-1,)) + pos
+            
+            tpos = np.array([self._nonfluents[f'TX___{dk}'], 
+                             self._nonfluents[f'TY___{dk}'], 
+                             self._nonfluents[f'TZ___{dk}']])
+            
+            self.ax.scatter(*pos, c='black', s=scale * self._nonfluents['R'])
+            self.ax.scatter(*motor1, c='black', s=scale * self._nonfluents['R'])
+            self.ax.scatter(*motor2, c='black', s=scale * self._nonfluents['R'])
+            self.ax.scatter(*motor3, c='black', s=scale * self._nonfluents['R'])
+            self.ax.scatter(*motor4, c='black', s=scale * self._nonfluents['R'])
+            self.ax.plot(*zip(pos, motor1), c='gray')
+            self.ax.plot(*zip(pos, motor2), c='gray')
+            self.ax.plot(*zip(pos, motor3), c='gray')
+            self.ax.plot(*zip(pos, motor4), c='gray')
+            self.ax.scatter(*tpos, c='red', marker='>')
+            # self.ax.autoscale_view()
+        
+        pygame.time.wait(self._wait_time)
+        
+        img = self.convert2img()
+        
+        self.ax.cla()
+        
+        return img
+
