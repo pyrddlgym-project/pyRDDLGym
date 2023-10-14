45ea48ae532306df5201d10eab1aaa01c388c2c9
Mike Gimelfarb <35513382+mike-gimelfarb@users.noreply.github.com>
2023-10-14 07:53:30 -0400

    Update ICAPSEvaluationScript.py

--- pyRDDLGym/ICAPSEvaluationScript.py
+++ pyRDDLGym/ICAPSEvaluationScript.py
@@ -1,19 +1,31 @@
+from datetime import datetime
+import jax
 import json
 import os
 import sys
 
 from pyRDDLGym.Core.Env.RDDLEnv import RDDLEnv
-from pyRDDLGym.Core.Jax.JaxRDDLBackpropPlanner import load_config
+from pyRDDLGym.Core.Gurobi.GurobiRDDLPlanner import GurobiStraightLinePlan
+from pyRDDLGym.Core.Gurobi.GurobiRDDLPlanner import GurobiOnlineController
+from pyRDDLGym.Core.Jax.JaxParameterTuning import JaxParameterTuningSLPReplan
+from pyRDDLGym.Core.Jax.JaxRDDLBackpropPlanner import _parse_config_file, _load_config
 from pyRDDLGym.Core.Jax.JaxRDDLBackpropPlanner import JaxRDDLBackpropPlanner
 from pyRDDLGym.Core.Jax.JaxRDDLBackpropPlanner import JaxOnlineController
 
 from rddlrepository.Manager.RDDLRepoManager import RDDLRepoManager as RDDLRepoManager
 
+CPUS = 6
+GP_ITERS = 5
+EPISODES = 5
+
  
-def main_jaxplan(domain, instance):
+def main(method, domain, instance):
+    assert method in ['jax', 'gurobi']
     
     # create the environment
-    manager = RDDLRepoManager(rebuild=True)
+    filename = f'{method}_{domain}_{instance}'
+    manager = RDDLRepoManager()
+
     EnvInfo = manager.get_problem(domain)    
     env = RDDLEnv(domain=EnvInfo.get_domain(),
                   instance=EnvInfo.get_instance(instance),
@@ -23,23 +35,61 @@ def main_jaxplan(domain, instance):
     # load the config file with planner settings
     abs_path = os.path.dirname(os.path.abspath(__file__))
     config_path = os.path.join(abs_path, 'ICAPSConfigs', f'{domain}.cfg') 
-    planner_args, _, train_args = load_config(config_path)
+    config, args = _parse_config_file(config_path)
+    planner_args, plan_args, train_args = _load_config(config, args)
     
     # create the planning algorithm
-    planner = JaxRDDLBackpropPlanner(rddl=env.model, **planner_args)
-    policy = JaxOnlineController(planner, **train_args)
-    
+    if method == 'jax':
+        
+        # run hyper-parameter tuning
+        tuning = JaxParameterTuningSLPReplan(
+            env=env,
+            train_epochs=train_args['epochs'],
+            timeout_training=train_args['train_seconds'],
+            planner_kwargs=planner_args,
+            plan_kwargs=plan_args,
+            num_workers=CPUS,
+            gp_iters=GP_ITERS,
+            eval_trials=EPISODES)
+        params = tuning.tune(key=jax.random.PRNGKey(int(datetime.now().timestamp())),
+                             filename=f'gp_{filename}')
+        
+        # update config with optimal hyper-parameters
+        args['method_kwargs']['initializer'] = 'normal'
+        args['method_kwargs']['initializer_kwargs'] = {'stddev': params['std']}
+        args['optimizer_kwargs']['learning_rate'] = params['lr']
+        args['logic_kwargs']['weight'] = params['w']
+        args['policy_hyperparams'] = {k: params['wa'] for k in args['policy_hyperparams']}
+        args['rollout_horizon'] = params['T']     
+        planner_args, plan_args, train_args = _load_config(config, args)
+        
+        # solve planning problem with new optimal parameters
+        planner = JaxRDDLBackpropPlanner(rddl=env.model, **planner_args)
+        policy = JaxOnlineController(planner, **train_args)
+        ground_state = False
+        
+    elif method == 'gurobi':
+        
+        # solve planning problem with new optimal parameters
+        policy = GurobiOnlineController(
+            rddl=env.model,
+            plan=GurobiStraightLinePlan(),
+            rollout_horizon=planner_args['rollout_horizon'],
+            model_params={'NonConvex': 2, 'OutputFlag': 0})
+        ground_state = True
+        
     # evaluation
-    result = policy.evaluate(env, verbose=True, episodes=10, ground_state=False)
+    result = policy.evaluate(
+        env, verbose=True, episodes=EPISODES, ground_state=ground_state)
     
     # dump all history to files
-    with open(f'JaxPlan_{domain}_{instance}.json', 'w') as fp:
-        json.dump(result, fp, sort_keys=True, indent=4)        
+    with open(f'{filename}.json', 'w') as fp:
+        json.dump(result, fp, indent=4)
     env.close()
 
 
 if __name__ == '__main__':
-    args = sys.argv[1:]
-    domain, instance = args[0], args[1]
-    main_jaxplan(domain, instance)
+    method, domain, instance = 'jax', 'Wildfire_MDP_ippc2014', '1'
+    # method, domain, instance = sys.argv[1:4]
+    main(method, domain, instance)
     
