43ea90d864ebfbbba2e21b4f2e09e8db8f5bf40e
Mike Gimelfarb <35513382+mike-gimelfarb@users.noreply.github.com>
2023-12-22 03:51:40 -0500

    Update runprost.sh

--- pyRDDLGym/Docker/runprost.sh
+++ pyRDDLGym/Docker/runprost.sh
@@ -2,7 +2,7 @@
 # usage: source runprost.sh <container name> <rddl path> <rounds> <prost arguments> <destination>
 
 # run the container
-docker run --name $1 --mount type=bind,source=$2,target=/RDDL prost $3 $4
+docker run --name $1 --mount type=bind,source=$2,target=/RDDL prost $3 "$4"
 	
 # copy the log files from container to local system
 docker cp $1:/OUTPUTS/ $5
