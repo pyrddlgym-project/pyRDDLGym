2b1c20ef26128bec3294dd6916c7a949e4fb230b
Mike Gimelfarb <35513382+mike-gimelfarb@users.noreply.github.com>
2024-01-29 22:37:46 -0500

    Added run example script for stable baselines

--- pyRDDLGym/examples/run_rl.py
+++ pyRDDLGym/examples/run_rl.py
@@ -2,7 +2,7 @@
     
 The syntax for running this example is:
 
-    python RLExample.py <domain> <instance> <steps>
+    python run_rl.py <domain> <instance> <steps>
     
 where:
     <domain> is the name of a domain located in the /Examples directory
@@ -12,19 +12,17 @@ where:
 import sys
 from stable_baselines3 import PPO
 
-from pyRDDLGym.Core.Env.RDDLEnv import RDDLEnv
-from pyRDDLGym.Core.Policies.Agents import BaseAgent
-from pyRDDLGym.Examples.ExampleManager import ExampleManager
+import pyRDDLGym
+from pyRDDLGym.baselines.stable_baselines.env import RDDLStableBaselinesEnv
+from pyRDDLGym.core.policy import BaseAgent
 
     
 def main(domain, instance, steps):
     
     # set up the environment
-    info = ExampleManager.GetEnvInfo(domain)    
-    env = RDDLEnv.build(info, instance, 
-                        enforce_action_constraints=True, 
-                        new_gym_api=True, 
-                        compact_action_space=True)
+    env = pyRDDLGym.make(domain, instance, 
+                         enforce_action_constraints=True,
+                         base_class=RDDLStableBaselinesEnv)
     
     # train the PPO agent
     model = PPO('MultiInputPolicy', env, verbose=1)
@@ -45,7 +43,8 @@ def main(domain, instance, steps):
 if __name__ == "__main__":
     args = sys.argv[1:]
     if len(args) < 3:
-        print('python RLExample.py <domain> <instance> <steps>')
+        print('python run_rl.py <domain> <instance> <steps>')
         exit(1)
     domain, instance, steps = args[:3]
+    steps = int(steps)
     main(domain, instance, steps)
