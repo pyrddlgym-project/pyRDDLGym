bc27915f76d9e1877e9a3d2130854e8d49a02bca
Mike Gimelfarb <35513382+mike-gimelfarb@users.noreply.github.com>
2024-02-01 18:52:49 -0500

    run_rl example is significantly generalized, supports algorithm selection

--- pyRDDLGym/examples/run_rl.py
+++ pyRDDLGym/examples/run_rl.py
@@ -2,49 +2,55 @@
     
 The syntax for running this example is:
 
-    python run_rl.py <domain> <instance> <steps>
+    python run_rl.py <domain> <instance> <method> [<steps>] [<learning_rate>]
     
 where:
     <domain> is the name of a domain located in the /Examples directory
     <instance> is the instance number
+    <method> is the algorithm to train (e.g. PPO, DQN etc.)
     <steps> is the number of trials to simulate for training
+    <learning_rate> is the learning rate to use to train the agent
 '''
 import sys
-from stable_baselines3 import PPO
+from stable_baselines3 import *
 
 import pyRDDLGym
 from pyRDDLGym.baselines.stable_baselines.env import StableBaselinesRDDLEnv
-from pyRDDLGym.core.policy import BaseAgent
+from pyRDDLGym.baselines.stable_baselines.agent import StableBaselinesAgent
 
-    
-def main(domain, instance, steps):
+METHODS = {'a2c': A2C, 'ddpg': DDPG, 'dqn': DQN, 'ppo': PPO, 'sac': SAC, 
+           'td3': TD3}
+
+def main(domain, instance, method, steps=200000, learning_rate=None):
     
     # set up the environment
     env = pyRDDLGym.make(domain, instance, 
-                         enforce_action_constraints=True,
-                         base_class=StableBaselinesRDDLEnv)
+                         base_class=StableBaselinesRDDLEnv,
+                         enforce_action_constraints=True)
     
     # train the PPO agent
-    model = PPO('MultiInputPolicy', env, verbose=1)
+    kwargs = {'verbose': 1}
+    if learning_rate is not None:
+        kwargs['learning_rate'] = learning_rate
+    model = METHODS[method]('MultiInputPolicy', env, **kwargs)    
     model.learn(total_timesteps=steps)
     
-    # container to hold trained PPO agent
-    class PPOAgent(BaseAgent):
-        use_tensor_obs = True
-        
-        def sample_action(self, state):
-            return model.predict(state)[0]
-        
-    # evaluate
-    PPOAgent().evaluate(env, episodes=1, verbose=True, render=True)
+    # wrap the agent in a RDDL policy and evaluate
+    ppo_agent = StableBaselinesAgent(model)
+    ppo_agent.evaluate(env, episodes=1, verbose=True, render=True)
+    
     env.close()
         
         
 if __name__ == "__main__":
     args = sys.argv[1:]
     if len(args) < 3:
-        print('python run_rl.py <domain> <instance> <steps>')
+        print('python run_rl.py <domain> <instance> <method> [<steps>] [<learning_rate>]')
         exit(1)
-    domain, instance, steps = args[:3]
-    steps = int(steps)
-    main(domain, instance, steps)
+    if args[2] not in METHODS:
+        print(f'<method> in {set(METHODS.keys())}')
+        exit(1)
+    kwargs = {'domain': args[0], 'instance': args[1], 'method': args[2]}
+    if len(args) >= 4: kwargs['steps'] = int(args[3])
+    if len(args) >= 5: kwargs['learning_rate'] = float(args[4])
+    main(**kwargs)
