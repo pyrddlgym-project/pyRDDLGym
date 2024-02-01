d54cf56b59a5eae34b6e8398c1952d2d24636fe5
Mike Gimelfarb <35513382+mike-gimelfarb@users.noreply.github.com>
2024-02-01 12:28:50 -0500

    Move xadd files to better location, rename, port API changes to level analysis

---
+++ pyRDDLGym/core/xadd/levels.py
@@ -0,0 +1,60 @@
+from typing import cast, Dict, List, Union
+from xaddpy.xadd import XADD
+
+from pyRDDLGym.core.compiler.levels import RDDLLevelAnalysis
+from pyRDDLGym.core.compiler.model import RDDLPlanningModel
+from pyRDDLGym.core.parser.expr import Expression
+
+from pyRDDLGym.xadd.model import RDDLModelXADD
+
+
+class RDDLLevelAnalysisXADD(RDDLLevelAnalysis):
+    
+    def __init__(self, model: RDDLPlanningModel, 
+                 allow_synchronous_state: bool = True) -> None:
+        super().__init__(model, allow_synchronous_state)
+        
+        self._model = cast(RDDLModelXADD, model)
+        self._context: XADD = self._model._context
+    
+    def _update_call_graph(self, graph: Dict[str, List[str]], 
+                           cpf: str, 
+                           expr: Union[int, Expression]):
+        if isinstance(expr, Expression):
+            super()._update_call_graph(graph, cpf, expr)
+        else:
+            rddl = self._model
+            var_set = self._context.collect_vars(expr)
+            for v in var_set:
+                v_name = rddl._sympy_var_name_to_var_name.get(str(v), str(v))
+                if v_name not in rddl.non_fluents and v not in rddl.rvs:
+                    graph.setdefault(cpf, set()).add(v_name)
+    
+    def build_call_graph(self) -> Dict[str, List[str]]:
+        rddl = self._model
+        
+        # compute call graph of CPs and check validity
+        cpf_graph = {}
+        for (name, expr) in rddl.cpfs.items():
+            self._update_call_graph(cpf_graph, name, expr)
+            if name not in cpf_graph:
+                cpf_graph[name] = set()
+        self._validate_dependencies(cpf_graph)
+        self._validate_cpf_definitions(cpf_graph)
+        
+        # check validity of reward, constraints, termination
+        for (name, exprs) in (
+            ('reward', [rddl.reward]),
+            ('precondition', rddl.preconditions),
+            ('invariant', rddl.invariants),
+            ('termination', rddl.terminations)
+        ):
+            call_graph_expr = {}
+            for expr in exprs:
+                if isinstance(expr, int):
+                    self._update_call_graph(call_graph_expr, name, expr)
+                else:
+                    super()._update_call_graph(call_graph_expr, name, expr)
+            self._validate_dependencies(call_graph_expr)
+            
+        return cpf_graph
