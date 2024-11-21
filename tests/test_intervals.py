7fb51212a031b92dc68c3384c7e783f34fd1087a
Daniel Dias <danielbpdias@gmail.com>
2024-11-21 16:21:35 -0300

    update tests and setup

--- tests/test_intervals.py
+++ tests/test_intervals.py
@@ -1,17 +1,19 @@
+import os
 import numpy as np
 
 import pyRDDLGym
 from pyRDDLGym.core.intervals import RDDLIntervalAnalysis
 
-TEST_DOMAIN = './tests/data/intervalanalysis/domain.rddl'
-TEST_INSTANCE = './tests/data/intervalanalysis/instance.rddl'
+ROOT_DIR = os.path.dirname(__file__)
 
-def get_action_bounds(env, policy):
-    action_bounds = None
-    
-    if policy != 'random':
-        return action_bounds
-    
+TEST_DOMAIN = f'{ROOT_DIR}/data/intervalanalysis/domain.rddl'
+TEST_INSTANCE = f'{ROOT_DIR}/data/intervalanalysis/instance.rddl'
+
+##################################################################################
+# Helper functions
+##################################################################################
+
+def get_action_bounds_from_env(env):    
     action_bounds = {}
     for action, prange in env.model.action_ranges.items():
         lower, upper = env._bounds[action]
@@ -22,21 +24,30 @@ def get_action_bounds(env, policy):
         
     return action_bounds
 
-def perform_interval_analysis(domain, instance, policy):
+def perform_interval_analysis(domain, instance, action_bounds = None, state_bounds = None):
     env = pyRDDLGym.make(domain, instance, vectorized=True)
     
-    action_bounds = get_action_bounds(env, policy)
+    if action_bounds is None:
+        action_bounds = get_action_bounds_from_env(env)
     
     analysis = RDDLIntervalAnalysis(env.model)
-    bounds = analysis.bound(action_bounds=action_bounds, per_epoch=True)
+    bounds = analysis.bound(action_bounds=action_bounds, state_bounds=state_bounds, per_epoch=True)
     
     env.close()
     
     return bounds
 
 
-def test_interval_analysis_simple_case():
-    bounds = perform_interval_analysis(TEST_DOMAIN, TEST_INSTANCE, 'random')   
+##################################################################################
+# Test definitions
+##################################################################################
+
+def test_simple_case():
+    ''' Evaluate if the interval propagation works well to a simple use case,
+        with a real-valued state fluent and the reward function,
+        without setting up the action and state bounds.
+    '''
+    bounds = perform_interval_analysis(TEST_DOMAIN, TEST_INSTANCE)   
     
     reward_lower, reward_upper = bounds['reward']
     np.testing.assert_array_equal(reward_lower, [0.0, -1.0])
@@ -46,3 +57,47 @@ def test_interval_analysis_simple_case():
         
     np.testing.assert_array_equal(realstatefluent_lower.flatten(), [-1.0, -2.0])
     np.testing.assert_array_equal(realstatefluent_upper.flatten(), [1.0, 2.0])
+    
+def test_action_bounds():
+    ''' Evaluate if the interval propagation works well to a simple use case,
+        with a real-valued state fluent and the reward function,
+        setting up the action bounds.
+    '''
+    action_bounds = {
+        'actionfluent': ( np.array([ -0.5 ]), np.array([ 0.5 ]) )
+    }
+    
+    bounds = perform_interval_analysis(TEST_DOMAIN, TEST_INSTANCE, action_bounds)   
+    
+    reward_lower, reward_upper = bounds['reward']
+    np.testing.assert_array_equal(reward_lower, [0.0, -0.5])
+    np.testing.assert_array_equal(reward_upper, [0.0, 0.5])
+    
+    realstatefluent_lower, realstatefluent_upper = bounds['realstatefluent']
+        
+    np.testing.assert_array_equal(realstatefluent_lower.flatten(), [-0.5, -1.0])
+    np.testing.assert_array_equal(realstatefluent_upper.flatten(), [0.5, 1.0])
+    
+def test_state_bounds():
+    ''' Evaluate if the interval propagation works well to a simple use case,
+        with a real-valued state fluent and the reward function,
+        setting up the state bounds.
+    '''
+    state_bounds = {
+        'statefluent': ( np.array([ -0.5 ]), np.array([ 0.5 ]) )
+    }
+    
+    action_bounds = {
+        'actionfluent': ( np.array([ -0.1 ]), np.array([ 0.1 ]) )
+    }
+    
+    bounds = perform_interval_analysis(TEST_DOMAIN, TEST_INSTANCE, action_bounds, state_bounds)   
+    
+    reward_lower, reward_upper = bounds['reward']
+    np.testing.assert_array_equal(reward_lower, [-0.5, -0.6])
+    np.testing.assert_array_equal(reward_upper, [0.5, 0.6])
+    
+    realstatefluent_lower, realstatefluent_upper = bounds['realstatefluent']
+        
+    np.testing.assert_array_equal(realstatefluent_lower.flatten(), [-0.5, -1.0])
+    np.testing.assert_array_equal(realstatefluent_upper.flatten(), [0.5, 1.0])
