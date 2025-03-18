Fixed issues in chart viz scaling for large instances

pyRDDLGym/core/visualizer/chart.py
@@ -13,19 +13,19 @@ class ChartVisualizer(BaseViz):
                  steps_history=None,
                  figure_size=(10, 10),
                  dpi=100,
-                 fontsize=10,
-                 markersize=4,
+                 fontsize=10, minfontsize=4,
+                 markersize=3, linewidth=1.,
                  boolcol=['red', 'green'],
-                 loccol='black',
                  scale_full_history: bool=True,
                  ranges=None) -> None:
         self._model = model
         self._figure_size = figure_size
         self._dpi = dpi
         self._fontsize = fontsize
+        self._minfontsize = minfontsize
         self._markersize = markersize
+        self._linewidth = linewidth
         self._boolcol = boolcol
-        self._loccol = loccol
         self._scale_full_history = scale_full_history
         self._ranges = ranges
 
@@ -42,25 +42,54 @@ class ChartVisualizer(BaseViz):
         self._historic_min_max = {}
         for (state, values) in self._model.state_fluents.items():
             values = np.atleast_1d(values)
-            self._state_hist[state] = np.full(shape=(len(values), self._steps),
-                                              fill_value=np.nan)
+            self._state_hist[state] = np.full(
+                shape=(len(values), self._steps), fill_value=np.nan)
             self._state_shapes[state] = self._model.object_counts(
                 self._model.variable_params[state])
             self._labels[state] = list(map(
-                ','.join,
-                self._model.ground_types(self._model.variable_params[state])
-            ))
+                ','.join, self._model.ground_types(self._model.variable_params[state])))
             self._historic_min_max[state] = (np.inf, -np.inf)
         self._step = 0
         self._drawn_artists = False
-    
+
+    def adjust_tick_label_size(self, ax, labels):
+        fig = ax.figure
+        dpi = fig.dpi
+        _, figure_height = fig.get_size_inches()
+        scaling_factor = (figure_height * dpi) / len(labels)
+        valid_font = scaling_factor / 5 >= self._minfontsize
+        if valid_font:
+            font_size = max(self._minfontsize, min(self._fontsize, scaling_factor / 5))
+            ax.tick_params(axis='y', labelsize=font_size)
+            ax.yaxis.set_ticks([0, len(labels)])
+            ax.yaxis.set(ticks=np.arange(0.5, len(labels), 1), ticklabels=labels)
+            ax.set_yticklabels(labels)
+
+    def adjust_legend_font_size(self, ax):
+        legend = ax.get_legend()
+        if legend:
+            fig = ax.figure
+            dpi = fig.dpi
+            _, figure_height = fig.get_size_inches()
+            num_labels = len(legend.get_texts())
+            if num_labels:
+                available_height_pixels = figure_height * dpi
+                font_size = max(self._minfontsize, min(
+                    self._fontsize, available_height_pixels / (num_labels * 10)))
+                for text in legend.get_texts():
+                    text.set_fontsize(font_size)
+                legend.handletextpad = 0
+                legend.borderaxespad = 0
+                legend.borderpad = 0
+                for handle in legend.legend_handles:
+                    handle.set_markersize(font_size / 4)
+
     def _setup(self):
         
         # prepare plot
-        self._fig, self._ax = plt.subplots(len(self._state_hist), 1,
-                                           squeeze=True,
-                                           figsize=self._figure_size,
-                                           sharex=True)
+        self._fig, self._ax = plt.subplots(
+            len(self._state_hist), 1,
+            squeeze=True, figsize=self._figure_size, sharex=True)
         if len(self._state_hist) == 1:
             self._ax = (self._ax,)
         self._fig.canvas.draw()
@@ -72,20 +101,18 @@ class ChartVisualizer(BaseViz):
             self._lines[state] = []
             
             # title, labels and cursor line
-            self._ax[y].xaxis.label.set_fontsize(self._fontsize)
-            self._ax[y].yaxis.label.set_fontsize(self._fontsize)
             if y == len(self._state_hist) - 1:
                 self._ax[y].set_xlabel('decision epoch')
-            self._ax[y].set_ylabel(state, rotation=15)
+            self._ax[y].set_ylabel(state, rotation=90)
+            self._ax[y].xaxis.label.set_fontsize(self._fontsize)
+            self._ax[y].yaxis.label.set_fontsize(self._fontsize)
+            self._ax[y].tick_params(axis='x', labelsize=self._fontsize)            
             
             # plot boolean fluent
             if self._model.variable_ranges[state] == 'bool':
                 line = self._ax[y].pcolormesh(values,
-                                              edgecolors=self._loccol,
-                                              linewidth=0.5,
                                               cmap=matplotlib.colors.ListedColormap(self._boolcol),
-                                              vmin=0,
-                                              vmax=1)
+                                              vmin=0, vmax=1)
                 self._lines[state].append(line)
                 
                 # set the legend colors
@@ -94,43 +121,36 @@ class ChartVisualizer(BaseViz):
                     matplotlib.patches.Patch(color=self._boolcol[1], label='true')
                 ]
                 self._ax[y].legend(handles=patches, loc='upper right')
-                
-                # set the state variables to the y-axis labels
-                labels = self._labels[state]
-                self._ax[y].yaxis.set_ticks([0, len(labels)])
-                self._ax[y].yaxis.set(ticks=np.arange(0.5, len(labels), 1),
-                                      ticklabels=labels)
-                self._ax[y].set_yticklabels(labels,
-                                            fontdict={"fontsize": self._fontsize},
-                                            rotation=30)
-            
+                self.adjust_tick_label_size(self._ax[y], self._labels[state])
+
             # plot real fluent
             elif self._model.variable_ranges[state] == 'real':
                 for (i, var) in enumerate(self._labels[state]):
                     line = self._ax[y].plot(values[i,:],
-                                            'o-',
-                                            markersize=self._markersize,
+                                            'o-', markersize=self._markersize,
+                                            linewidth=self._linewidth,
                                             label=var)[0]
                     self._lines[state].append(line)
                 self._ax[y].set_xlim([0, values[i,:].size])
-                self._ax[y].legend(loc='upper right')
             
             # plot int and object fluent
             else:
                 for (i, var) in enumerate(self._labels[state]):
-                    line = self._ax[y].plot(np.arange(values[i,:].size),
-                                            values[i,:],
-                                            linestyle=':',
-                                            marker='o',
-                                            markersize=self._markersize,
+                    line = self._ax[y].plot(np.arange(values[i,:].size), values[i,:],
+                                            linestyle=':', linewidth=self._linewidth,
+                                            marker='o', markersize=self._markersize, 
                                             markerfacecolor='none',
                                             label=var)[0]
                     self._lines[state].append(line)
                 self._ax[y].set_xlim([0, values[i,:].size])
-                if var != '':
-                    self._ax[y].legend(loc='upper right')
         plt.tight_layout()
-                    
+
+        # prepare legends
+        for (y, (state, values)) in enumerate(self._state_hist.items()):
+            if self._model.variable_ranges[state] != 'bool' and len(self._labels[state]) > 1:
+                self._ax[y].legend(loc='upper right', labelspacing=0)
+                self.adjust_legend_font_size(self._ax[y])
+
         self._backgrounds = [self._fig.canvas.copy_from_bbox(ax.bbox) for ax in self._ax]
         self._drawn_artists = True
         